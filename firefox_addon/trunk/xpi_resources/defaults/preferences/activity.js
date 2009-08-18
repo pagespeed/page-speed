99364a8b64e4608da5ead58e30bee5ee28133e08
bmcquade <bmcquade@google.com>
2009-08-18 14:49:12 +0000

    Hide paint event capture behind a preference (disable by default).
    r=skerner

--- firefox_addon/trunk/xpi_resources/defaults/preferences/activity.js
+++ firefox_addon/trunk/xpi_resources/defaults/preferences/activity.js
@@ -7,3 +7,4 @@ pref('extensions.Activity.ui_data_available_instantaneous', true);
 
 pref('extensions.Activity.profile_js', true);
 pref('extensions.Activity.canvas_width_px', 250);
+pref('extensions.Activity.enable_screen_snapshots', false);
