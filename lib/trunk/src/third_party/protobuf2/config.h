d3abfbaeacd985c106891ad63fe9ef3f85adf336
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2009-08-18 20:31:48 +0000

    Merge visual studio config.h with the current config.h.
    r=avd

--- lib/trunk/src/third_party/protobuf2/config.h
+++ lib/trunk/src/third_party/protobuf2/config.h
@@ -1,3 +1,37 @@
+#if defined(_WINDOWS)
+
+/* The code in this section comes from
+ * http://code.google.com/p/protobuf/source/browse/tags/2.2.0/vsprojects/config.h
+ */
+
+/* protobuf config.h for MSVC.  On other platforms, this is generated
+ * automatically by autoheader / autoconf / configure. */
+
+/* the location of <hash_map> */
+#define HASH_MAP_H <hash_map>
+
+/* the namespace of hash_map/hash_set */
+#if _MSC_VER < 1310
+#define HASH_NAMESPACE std
+#else
+#define HASH_NAMESPACE stdext
+#endif
+
+/* the location of <hash_set> */
+#define HASH_SET_H <hash_set>
+
+/* define if the compiler has hash_map */
+#define HAVE_HASH_MAP 1
+
+/* define if the compiler has hash_set */
+#define HAVE_HASH_SET 1
+
+/* define if you want to use zlib.  See readme.txt for additional
+ * requirements. */
+// #define HAVE_ZLIB 1
+
+#else
+
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
@@ -135,4 +169,6 @@
 #endif
 #ifndef _TANDEM_SOURCE
 # define _TANDEM_SOURCE 1
+#endif
+
 #endif
