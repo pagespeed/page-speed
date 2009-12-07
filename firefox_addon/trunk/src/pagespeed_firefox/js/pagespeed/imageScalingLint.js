56a07156b238977be6fe6b1bef85c540893cb5b3
avd@google.com <avd@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2009-12-07 19:30:09 +0000

    Split image compression and html image scaling into 2 rules.
    r=mdsteele

--- firefox_addon/trunk/src/pagespeed_firefox/js/pagespeed/imageScalingLint.js
+++ firefox_addon/trunk/src/pagespeed_firefox/js/pagespeed/imageScalingLint.js
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * @fileoverview Queries image compression component which uses
- * optipng and libjpeg to determine which images on the page can be
- * compressed further.
+ * @fileoverview Tests for images that are downsampled in HTML.
  *
  * @author Tony Gentilcore
  * @author Bryan McQuade
@@ -24,36 +22,92 @@
 (function() {  // Begin closure
 
 /**
- * Minimum number of bytes we expect to save when compressing an image
- * before we warn the user about it.
- * @type {number}
- */
-var MIN_BYTES_SAVED = 25;
-
-/**
- * Name of the directory where we store compressed images.
- * @type {string}
- */
-var COMPRESSED_IMAGE_DIR_NAME = 'page-speed-images';
-
-/**
- * Permissions of the directory where we store compressed images.
- * @type {number}
- */
-var COMPRESSED_IMAGE_DIR_PERMISSIONS = 0755;
-
-/**
- * Data structure to hold the result of an image compression attempt.
- * @param {string} url The url of the compressed version of the image.
- * @param {number} size the size, in bytes, of the compressed image.
+ * Data structure to hold image natural size and scaling information.
  * @constructor
  */
-function ImageCompressionData(url, size) {
+function ImageScaleData() {
   /** @type {number} */
-  this.compressedSize = size;
+  this.naturalWidth = -1;
 
-  /** @type {string} */
-  this.compressedUrl = url;
+  /** @type {number} */
+  this.naturalHeight = -1;
+
+  /** @type {number} */
+  this.clientWidth = -1;
+
+  /** @type {number} */
+  this.clientHeight = -1;
+}
+
+/**
+ * @param {number} naturalWidth The actual width of the image.
+ * @param {number} naturalHeight The actual height of the image.
+ * @param {number} clientWidth The width of the image in the HTML.
+ * @param {number} clientHeight The height of the image in the HTML.
+ */
+ImageScaleData.prototype.setResizable = function(
+    naturalWidth, naturalHeight, clientWidth, clientHeight) {
+  if (this.naturalHeight != -1 || this.naturalWidth != -1) {
+    // The image is already known to be resizable. We want to keep
+    // track of the largest instance of this image on the page, so
+    // check and see if the new client size is larger than the
+    // previously recorded client size.
+    if (naturalHeight != this.naturalHeight ||
+        naturalWidth != this.naturalWidth) {
+      // Sanity check that the size is the same.
+      throw new Error('Mismatched width/height parameters!');
+    }
+  }
+
+  this.naturalWidth = naturalWidth;
+  this.naturalHeight = naturalHeight;
+  this.clientWidth = Math.min(Math.max(this.clientWidth, clientWidth),
+                              naturalWidth);
+  this.clientHeight = Math.min(Math.max(this.clientHeight, clientHeight),
+                               naturalHeight);
+};
+
+/**
+ * @return {boolean} whether or not the image is resizable.
+ */
+ImageScaleData.prototype.isResizable = function() {
+  return (this.clientWidth >= 0 && this.clientHeight >= 0 && 
+          (this.clientWidth < this.naturalWidth || 
+           this.clientHeight < this.naturalHeight));
+};
+
+/**
+ * Find all downsampled images.
+ * @param {Array.<string>} imgs Array of image URLs referenced on
+ *     this page.
+ * @param {ImageDataContainer} results A map from url to ImageData objects.
+ */
+function findResizableImages(imgs, results) {
+  if (!imgs) {
+    return;
+  }
+
+  // Keep track of the least-scaled version of each image on the
+  // page. For instance, if an image appears twice, once unscaled and
+  // another time scaled down, we should not penalize the page, since
+  // they are using the unscaled version somewhere. However, if an
+  // image is used more than once, and is scaled each time, we should
+  // penalize them based on the largest scaled size.
+  for (var img in imgs) {
+    for (var i = 0, len = imgs[img].elements.length; i < len; ++i) {
+      var elem = imgs[img].elements[i];
+      if (elem.tagName != 'IMG') continue;
+
+      if (elem.clientWidth && elem.clientHeight &&
+          elem.naturalWidth && elem.naturalHeight) {
+        var data = results.getData(img);
+        data.setResizable(elem.naturalWidth,
+                          elem.naturalHeight,
+                          elem.clientWidth,
+                          elem.clientHeight);
+      }
+    }
+  }
 }
 
 /**
@@ -67,20 +121,28 @@ function ImageData(url) {
   /** @type {number} */
   this.origSize = PAGESPEED.Utils.getResourceSize(url);
 
-  /** @type {ImageCompressionData?} */
-  this.compressionData = null;
+  /** @type {ImageScaleData?} */
+  this.scaleData = null;
 }
 
 /**
- * @param {ImageCompressionData} compressionData Object that holds
- * compression data info.
+ * @param {number} naturalWidth The actual width of the image.
+ * @param {number} naturalHeight The actual height of the image.
+ * @param {number} clientWidth The width of the image in the HTML.
+ * @param {number} clientHeight The height of the image in the HTML.
  */
-ImageData.prototype.setCompressible = function(compressionData) {
-  this.compressionData = compressionData;
-};
+ImageData.prototype.setResizable = function(
+  naturalWidth, naturalHeight, clientWidth, clientHeight) {
+  if (!this.scaleData) {
+    this.scaleData = new ImageScaleData;
+  }
+  this.scaleData.setResizable(naturalWidth, naturalHeight,
+			      clientWidth, clientHeight);
+}
 
 /**
- * @return {number} the number of bytes saved by compression.
+ * @return {number} the number of bytes saved by compression and
+ *     resizing.
  */
 ImageData.prototype.getBytesSaved = function() {
   return this.origSize * this.getCompressionAmount();
@@ -92,31 +154,43 @@ ImageData.prototype.getBytesSaved = function() {
  *     compression down to zero bytes).
  */
 ImageData.prototype.getCompressionAmount = function() {
-  var compressionMultiplier = 1;
-  if (this.compressionData) {
-    compressionMultiplier = this.compressionData.compressedSize / this.origSize;
+  var resizingMultiplier = 1;
+  var scaleData = this.scaleData;
+  if (scaleData && scaleData.isResizable()) {
+    if (scaleData.clientWidth < scaleData.naturalWidth) {
+      resizingMultiplier *= scaleData.clientWidth / scaleData.naturalWidth;
+    }
+    if (scaleData.clientHeight < scaleData.naturalHeight) {
+      resizingMultiplier *= scaleData.clientHeight / scaleData.naturalHeight;
+    }
   }
-  return 1 - compressionMultiplier;
+  return 1 - resizingMultiplier;
 };
 
 /**
  * @return {string} A human-readable results string.
  */
 ImageData.prototype.buildResultString = function() {
-  var compressible = !!this.compressionData;
-  if (!compressible) {
+  var resizable = !!this.scaleData && this.scaleData.isResizable();
+  if (!resizable) {
     return '';
   }
-  var compressString = ['. See ', this.compressionData.compressedUrl].join('');
-  return ['Compressing ',
-          this.url,
-          ' would save ',
+
+  var scaleData = this.scaleData;
+  return [this.url,
+	  'is scaled in HTML from ',
+	  scaleData.naturalWidth,
+	  'x',
+          scaleData.naturalHeight,
+          ' to ',
+	  scaleData.clientWidth,
+	  'x',
+          scaleData.clientHeight,
+          '.  Serving a resized image could save ~',
           PAGESPEED.Utils.formatBytes(this.getBytesSaved()),
           ' (',
           PAGESPEED.Utils.formatPercent(this.getCompressionAmount()),
-          ' reduction)',
-          compressString,
-          '.'].join('');
+          ' reduction).'].join('');
 };
 
 /**
@@ -151,9 +225,6 @@ ImageDataContainer.prototype.getDataArray = function() {
   aData.sort(sortByBytesSaved);
   return aData;
 };
-
-var compressor = PAGESPEED.Utils.CCSV(
-    '@code.google.com/p/page-speed/ImageCompressor;1', 'IImageCompressor');
 
 // Sorts the compressed images by most bytes saved.
 function sortByBytesSaved(a, b) {
@@ -257,96 +328,9 @@ function prepareFileForCompression(url, extension) {
 }
 
 /**
- * @param {string} url The URL of the image we're trying to compress.
- * @param {ImageData} data the result of the compression attempt.
- */
-function tryToCompressImage(url, data) {
-  // Get the file extension for the file type we are going to compress
-  // this resource to (png, jpg, or null).
-  var extension = getCompressedFileExtension(url);
-  if (!extension) {
-    return;
-  }
-  var compressedFile = prepareFileForCompression(url, extension);
-  if (!compressedFile) {
-    return;
-  }
-  var compressedPath = compressedFile.path;
-
-  var success = false;
-  try {
-    if (extension == 'png') {
-      compressor.compressToPng(compressedPath, compressedPath);
-      success = true;
-    } else if (extension == 'jpg') {
-      compressor.compressJpeg(compressedPath, compressedPath);
-      success = true;
-    }
-  } finally {
-    if (!success) {
-      compressedFile.remove(true);
-    }
-  }
-
-  if (!compressedFile.exists()) {
-    return;
-  }
-
-  data.setCompressible(
-    new ImageCompressionData(PAGESPEED.Utils.getUrlForFile(compressedFile),
-			     compressedFile.fileSize));
-
-  // Make sure the temp file gets deleted when Firefox exits.
-  PAGESPEED.Utils.deleteTemporaryFileOnExit(compressedFile);
-}
-
-/**
- * Given a URL, return a function that tries to compress it.
- * @this PAGESPEED.LintRule
- * @param {string} url Url of the image to compress.
- * @return {Function} A function that will try to compress the image at
- *     the given Url.
- */
-function buildImageCompressionCallback(url) {
-  return function() {
-    try {
-      var data = this.results.getData(url);
-      tryToCompressImage(url, data);
-    } catch (e) {
-      // Unable to compress this image. Could be an animated gif,
-      // which can't be converted to png. Ignore and try to compress
-      // the next one.
-      PS_LOG('Unable to compress image ' + url);
-    }
-
-    return url;
-  };
-}
-
-/**
  * @this PAGESPEED.LintRule
  */
-var imageCompressionLint = function() {
-  if (!compressor) {
-    this.score = 'error';
-    var xulRuntime = PAGESPEED.Utils.CCSV(
-        '@mozilla.org/xre/app-info;1', 'nsIXULRuntime');
-    this.warnings = ['Unable to compress images on your platform: ',
-                     xulRuntime.OS, xulRuntime.XPCOMABI].join(' ');
-    return;
-  }
-
-  var imagesDir = PAGESPEED.Utils.getOutputDir(COMPRESSED_IMAGE_DIR_NAME);
-  if (!imagesDir) {
-    this.score = 'error';
-    this.warnings = ['Unable to compress images because the directory where ',
-                     'optimized images are saved can not be ',
-                     'accessed.  Click the black triangle in the Page Speed ',
-                     'tab and select a different path under "Save Optimized ',
-                     'Files To:".'].join('');
-    return;
-  }
-
+var imageScaleLint = function() {
   var urls = PAGESPEED.Utils.getResources('image', 'cssimage', 'favicon');
 
   // Shortcut to get out early if the page doesn't have any images.
@@ -357,13 +341,9 @@ var imageCompressionLint = function() {
 
   this.results = new ImageDataContainer();
 
-  // For each compressable image, add a function to do the compression.
-  for (var i = 0, len = urls.length; i < len; i++) {
-    // Add a continuation that will try to compress the image.
-    this.addContinuation(buildImageCompressionCallback(urls[i]));
-  }
-
   this.addContinuation(function() {
+    findResizableImages(PAGESPEED.Utils.getComponents().image, this.results);
+
     var aResults = this.results.getDataArray();
 
     var aWarnings = [];
@@ -372,10 +352,8 @@ var imageCompressionLint = function() {
     for (var i = 0, len = aResults.length; i < len; i++) {
       var data = aResults[i];
       origBytes += data.origSize;
-      if (data.getBytesSaved() > MIN_BYTES_SAVED) {
-        totalWastedBytes += data.getBytesSaved();
-        aWarnings.push(data.buildResultString());
-      }
+      totalWastedBytes += data.getBytesSaved();
+      aWarnings.push(data.buildResultString());
     }
 
     // Compute the score by looping through all image warnings and deducting
@@ -383,27 +361,17 @@ var imageCompressionLint = function() {
     if (aWarnings.length > 0) {
       if (totalWastedBytes && origBytes) {
         this.score -= 1.5 * parseInt(totalWastedBytes / origBytes * 100, 10);
-        this.warnings += ['There are ', PAGESPEED.Utils.formatBytes(origBytes),
-                          ' worth of images. Optimizing them could',
-                          ' save ~',
-                          PAGESPEED.Utils.formatBytes(totalWastedBytes),
-                          ' (',
-                          PAGESPEED.Utils.formatPercent(
-                              totalWastedBytes / origBytes),
-                          ' reduction).'].join('');
+        this.warnings += [
+	  'The following images are scaled in HTML.  ',
+	  'Serving resized images could save ~',
+          PAGESPEED.Utils.formatBytes(totalWastedBytes),
+          ' (',
+          PAGESPEED.Utils.formatPercent(
+            totalWastedBytes / origBytes),
+          ' reduction).'].join('');
       }
 
       var messages = PAGESPEED.Utils.formatWarnings(aWarnings);
-
-      // Remove the path from the text of the link to the compressed images, to
-      // make it more readable.
-      var imagesDir = PAGESPEED.Utils.getOutputDir(COMPRESSED_IMAGE_DIR_NAME);
-      if (imagesDir) {
-        var imageDirUrl = PAGESPEED.Utils.getUrlForFile(imagesDir);
-        var imageDirRegexp = new RegExp(
-            ['>', imageDirUrl, '[^<]*'].join(''), 'g');
-        messages = messages.replace(imageDirRegexp, '>compressed version');
-      }
       this.warnings += messages;
     }
   });
@@ -413,12 +381,12 @@ var imageCompressionLint = function() {
 
 PAGESPEED.LintRules.registerLintRule(
   new PAGESPEED.LintRule(
-    'Optimize images',
+    'Serve scaled images',
     PAGESPEED.PAYLOAD_GROUP,
-    'payload.html#CompressImages',
-    imageCompressionLint,
+    'payload.html#ScaleImages',
+    imageScaleLint,
     3.0,
-    'OptImgs'
+    'ScaleImgs'
   )
 );
 
