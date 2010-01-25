5942957d572fa4c4713fae66a99b5e7d67c9e40f
bmcquade <bmcquade@google.com>
2010-01-25 17:56:02 +0000

    Create 1.6 branch.

---
+++ lib/branches/lib-1.6.0/src/pagespeed/rules/enable_gzip_compression_test.cc
@@ -0,0 +1,280 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+
+#include "base/scoped_ptr.h"
+#include "pagespeed/core/pagespeed_input.h"
+#include "pagespeed/core/resource.h"
+#include "pagespeed/proto/pagespeed_output.pb.h"
+#include "pagespeed/rules/enable_gzip_compression.h"
+#include "pagespeed/rules/savings_computer.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using pagespeed::rules::compression_computer::ZlibComputer;
+using pagespeed::rules::EnableGzipCompression;
+using pagespeed::rules::SavingsComputer;
+using pagespeed::PagespeedInput;
+using pagespeed::Resource;
+using pagespeed::Result;
+using pagespeed::Results;
+using pagespeed::Savings;
+
+namespace {
+
+class EnableGzipCompressionTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    input_.reset(new PagespeedInput);
+  }
+
+  virtual void TearDown() {
+    input_.reset();
+  }
+
+  void AddTestResource(const char* url,
+                       const char* content_type,
+                       const char* content_encoding,
+                       const std::string& body) {
+    Resource* resource = new Resource;
+    resource->SetRequestUrl(url);
+    resource->SetRequestMethod("GET");
+    resource->SetRequestProtocol("HTTP");
+    resource->SetResponseStatusCode(200);
+    resource->SetResponseProtocol("HTTP/1.1");
+
+    if (content_type != NULL) {
+      resource->AddResponseHeader("Content-Type", content_type);
+    }
+
+    if (content_encoding != NULL) {
+      resource->AddResponseHeader("Content-Encoding", content_encoding);
+    }
+
+    resource->SetResponseBody(body);
+    input_->AddResource(resource);
+  }
+
+  void AddTestResource(const char* url,
+                       const char* content_type,
+                       const char* content_encoding,
+                       size_t content_length) {
+    std::string body;
+    body.append(content_length, ' ');
+    AddTestResource(url, content_type, content_encoding, body);
+  }
+
+  void AddFirstLargeHtmlResource(const char* charset,
+                                 bool gzip) {
+    std::string content_type = "text/html";
+    if (charset != NULL) {
+      content_type += "; charset=";
+      content_type += charset;
+    }
+    AddTestResource("http://www.test.com/",
+                    content_type.c_str(),
+                    gzip ? "gzip" : NULL,
+                    9000);
+  }
+
+  void AddFirstLargeHtmlResource(bool gzip) {
+    return AddFirstLargeHtmlResource(NULL, gzip);
+  }
+
+  void AddSecondLargeHtmlResource(bool gzip) {
+    AddTestResource("http://www.test.com/foo",
+                    "text/html",
+                    gzip ? "gzip" : NULL,
+                    4500);
+  }
+
+  void AddShortHtmlResource() {
+    AddTestResource("http://www.test.com/",
+                    "text/html",
+                    NULL,
+                    10);
+  }
+
+  void CheckNoViolations() {
+    CheckNoViolationsInternal(true);
+  }
+
+  void CheckErrorAndNoViolations() {
+    CheckNoViolationsInternal(false);
+  }
+
+  void CheckOneViolation(int expected_savings) {
+    CheckOneViolationInternal(new ZlibComputer(), expected_savings, true);
+  }
+
+  void CheckErrorAndOneViolation(SavingsComputer* computer,
+                                 int expected_savings) {
+    CheckOneViolationInternal(computer, expected_savings, false);
+  }
+
+  void CheckTwoViolations(int first_expected_savings,
+                          int second_expected_savings) {
+    EnableGzipCompression gzip_rule(new ZlibComputer());
+
+    Results results;
+    ASSERT_TRUE(gzip_rule.AppendResults(*input_, &results));
+    ASSERT_EQ(results.results_size(), 2);
+
+    const Result& result0 = results.results(0);
+    ASSERT_EQ(result0.savings().response_bytes_saved(), first_expected_savings);
+    ASSERT_EQ(result0.resource_urls_size(), 1);
+    ASSERT_EQ(result0.resource_urls(0), "http://www.test.com/");
+
+    const Result& result1 = results.results(1);
+    ASSERT_EQ(result1.savings().response_bytes_saved(),
+              second_expected_savings);
+    ASSERT_EQ(result1.resource_urls_size(), 1);
+    ASSERT_EQ(result1.resource_urls(0), "http://www.test.com/foo");
+  }
+
+ private:
+  void CheckNoViolationsInternal(bool expect_success) {
+    EnableGzipCompression gzip_rule(new ZlibComputer());
+
+    Results results;
+    ASSERT_EQ(expect_success, gzip_rule.AppendResults(*input_, &results));
+    ASSERT_EQ(results.results_size(), 0);
+  }
+
+  void CheckOneViolationInternal(SavingsComputer* computer,
+                                 int expected_savings,
+                                 bool expect_success) {
+    EnableGzipCompression gzip_rule(computer);
+
+    Results results;
+    ASSERT_EQ(expect_success, gzip_rule.AppendResults(*input_, &results));
+    ASSERT_EQ(results.results_size(), 1);
+
+    const Result& result = results.results(0);
+    ASSERT_EQ(result.savings().response_bytes_saved(), expected_savings);
+    ASSERT_EQ(result.resource_urls_size(), 1);
+    ASSERT_EQ(result.resource_urls(0), "http://www.test.com/");
+  }
+
+  scoped_ptr<PagespeedInput> input_;
+};
+
+TEST_F(EnableGzipCompressionTest, ViolationLargeHtmlNoGzip) {
+  AddFirstLargeHtmlResource(false);
+
+  CheckOneViolation(8956);
+}
+
+TEST_F(EnableGzipCompressionTest, ViolationLargeHtmlUtf8NoGzip) {
+  AddFirstLargeHtmlResource("utf-8", false);
+
+  CheckOneViolation(8956);
+}
+
+TEST_F(EnableGzipCompressionTest, NoViolationLargeHtmlGzip) {
+  AddFirstLargeHtmlResource(true);
+
+  CheckNoViolations();
+}
+
+TEST_F(EnableGzipCompressionTest, NoViolationSmallHtmlNoGzip) {
+  AddShortHtmlResource();
+
+  CheckNoViolations();
+}
+
+TEST_F(EnableGzipCompressionTest, NoViolationLargeNoContentTypeNoGzip) {
+  AddTestResource("http://www.test.com/", NULL, NULL, 9000);
+
+  CheckNoViolations();
+}
+
+TEST_F(EnableGzipCompressionTest, NoViolationLargeImageNoGzip) {
+  AddTestResource("http://www.test.com/", "image/jpeg", NULL, 9000);
+
+  CheckNoViolations();
+}
+
+TEST_F(EnableGzipCompressionTest, NoViolationLargeHtmlGzipSdch) {
+  AddTestResource("http://www.test.com/", "text/html", "gzip,sdch", 9000);
+
+  CheckNoViolations();
+}
+
+TEST_F(EnableGzipCompressionTest, NoViolationTwoHtmlGzip) {
+  AddFirstLargeHtmlResource(true);
+  AddSecondLargeHtmlResource(true);
+
+  CheckNoViolations();
+}
+
+TEST_F(EnableGzipCompressionTest, OneViolationTwoHtmlNoGzip) {
+  AddFirstLargeHtmlResource(false);
+  AddSecondLargeHtmlResource(true);
+
+  CheckOneViolation(8956);
+}
+
+TEST_F(EnableGzipCompressionTest, TwoViolationsTwoHtmlNoGzip) {
+  AddFirstLargeHtmlResource(false);
+  AddSecondLargeHtmlResource(false);
+
+  CheckTwoViolations(8956, 4460);
+}
+
+TEST_F(EnableGzipCompressionTest, NullComputer) {
+  ASSERT_DEATH(new EnableGzipCompression(NULL),
+               "SavingsComputer must be non-null.");
+}
+
+TEST_F(EnableGzipCompressionTest, BinaryResponseBody) {
+  std::string body;
+  body.append(9000, ' ');
+  body[0] = '\0';
+  AddTestResource("http://www.test.com/", "text/html", NULL, body);
+  CheckOneViolation(8955);
+}
+
+class FailAtSpecifiedIndexComputer : public SavingsComputer {
+ public:
+  explicit FailAtSpecifiedIndexComputer(int index)
+      : index_(index),
+        counter_(0) {}
+  ~FailAtSpecifiedIndexComputer() {}
+
+  virtual bool ComputeSavings(const Resource& resource, Savings* savings) {
+    savings->set_response_bytes_saved(10);
+    return (counter_++ != index_);
+  }
+
+ private:
+  const int index_;
+  int counter_;
+};
+
+TEST_F(EnableGzipCompressionTest, FailedComputationsNotAddedToResults) {
+  AddFirstLargeHtmlResource(false);
+  AddSecondLargeHtmlResource(false);
+
+  CheckErrorAndOneViolation(new FailAtSpecifiedIndexComputer(1), 10);
+}
+
+TEST_F(EnableGzipCompressionTest, FailedComputationsNotAddedToResults2) {
+  AddSecondLargeHtmlResource(false);
+  AddFirstLargeHtmlResource(false);
+
+  CheckErrorAndOneViolation(new FailAtSpecifiedIndexComputer(0), 10);
+}
+
+}  // namespace
---
+++ lib/branches/lib-1.6.0/src/pagespeed/rules/minimize_dns_lookups.cc
@@ -0,0 +1,189 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "pagespeed/rules/minimize_dns_lookups.h"
+
+#include <string>
+#include <vector>
+
+#include "base/logging.h"
+#include "pagespeed/core/formatter.h"
+#include "pagespeed/core/pagespeed_input.h"
+#include "pagespeed/core/resource.h"
+#include "pagespeed/proto/pagespeed_output.pb.h"
+
+// Unfortunately, including Winsock2.h before the other dependencies
+// causes a build error due to a conflict with the protobuf library,
+// so we are forced to push these includes to the bottom of the
+// include list. See
+// http://code.google.com/p/protobuf/issues/detail?id=44 for more
+// information.
+#if defined(_WINDOWS)
+#include <Winsock2.h>
+#else
+#include <arpa/inet.h>
+#endif
+
+namespace {
+
+bool IsAnIPAddress(const char* str) {
+  if (!str) {
+    return false;
+  }
+
+  if (inet_addr(str) != INADDR_NONE) {
+    return true;
+  }
+
+  // TODO: add support for IPv6 addresses. See
+  // http://code.google.com/p/page-speed/issues/detail?id=109 for more
+  // information.
+
+  return false;
+}
+
+}  // namespace
+
+namespace pagespeed {
+
+namespace rules {
+
+MinimizeDnsLookups::MinimizeDnsLookups() {
+}
+
+const char* MinimizeDnsLookups::name() const {
+  return "MinimizeDnsLookups";
+}
+
+const char* MinimizeDnsLookups::header() const {
+  return "Minimize DNS lookups";
+}
+
+const char* MinimizeDnsLookups::documentation_url() const {
+  return "rtt.html#MinimizeDNSLookups";
+}
+
+bool MinimizeDnsLookups::AppendResults(const PagespeedInput& input,
+                                       Results* results) {
+  // TODO This logic is wrong.  Right now, if you have two resources in
+  //      different domains, this rule will complain about both resources, and
+  //      claim a savings of one DNS request for each resource.  However, you
+  //      can't actually save two DNS requests by combining the resources --
+  //      you can only save one.
+
+  const HostResourceMap& host_resource_map = *input.GetHostResourceMap();
+
+  // Find resources that are the only (http, non-lazy-loaded) resource from
+  // their domain.
+  ResourceVector lone_dns_resources;
+  int num_relevant_hosts = 0;
+  for (HostResourceMap::const_iterator iter = host_resource_map.begin(),
+           end = host_resource_map.end();
+       iter != end;
+       ++iter) {
+    const std::string& host = iter->first;
+
+    // If the ip address is appears explicitly, no DNS lookup is required.
+    if (IsAnIPAddress(host.c_str())) {
+      continue;
+    }
+
+    const ResourceVector& resources = iter->second;
+
+    ResourceVector filtered;
+    for (ResourceVector::const_iterator resource_iter = resources.begin(),
+             resource_end = resources.end();
+         resource_iter != resource_end; ++resource_iter) {
+      const Resource* resource = *resource_iter;
+
+      // exclude non-http resources
+      const std::string& protocol = resource->GetProtocol();
+      if (protocol != "http" && protocol != "https") {
+        continue;
+      }
+
+      // exclude lazy-loaded resources
+      if (resource->IsLazyLoaded()) {
+        continue;
+      }
+
+      if (host.empty()) {
+        LOG(DFATAL) << "Empty host while processing "
+                    << resource->GetRequestUrl();
+      }
+      filtered.push_back(resource);
+    }
+
+    if (!filtered.empty()) {
+      ++num_relevant_hosts;
+      if (filtered.size() == 1) {
+        lone_dns_resources.push_back(*filtered.begin());
+      }
+    }
+  }
+
+  // Report a result for each lone resource (unless there is only one domain).
+  if (num_relevant_hosts > 1) {
+    for (ResourceVector::const_iterator iter = lone_dns_resources.begin(),
+             end = lone_dns_resources.end();
+         iter != end; ++iter) {
+      const Resource* resource = *iter;
+
+      Result* result = results->add_results();
+      result->set_rule_name(name());
+      result->add_resource_urls(resource->GetRequestUrl());
+
+      Savings* savings = result->mutable_savings();
+      savings->set_dns_requests_saved(1);
+    }
+  }
+
+  return true;
+}
+
+void MinimizeDnsLookups::FormatResults(const ResultVector& results,
+                                       Formatter* formatter) {
+  std::vector<std::string> violation_urls;
+  for (ResultVector::const_iterator iter = results.begin(),
+           end = results.end();
+       iter != end;
+       ++iter) {
+    const Result& result = **iter;
+
+    for (int idx = 0; idx < result.resource_urls_size(); idx++) {
+      violation_urls.push_back(result.resource_urls(idx));
+    }
+  }
+
+  if (violation_urls.empty()) {
+    return;
+  }
+
+  Formatter* body = formatter->AddChild(
+      "The domains of the following urls only serve one "
+      "resource each. If possible, avoid the extra DNS "
+      "lookups by serving these resources from existing domains.");
+
+  for (std::vector<std::string>::const_iterator iter = violation_urls.begin(),
+           end = violation_urls.end();
+       iter != end;
+       ++iter) {
+    Argument url(Argument::URL, *iter);
+    body->AddChild("$1", url);
+  }
+}
+
+}  // namespace rules
+
+}  // namespace pagespeed
