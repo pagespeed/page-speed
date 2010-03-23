5f40d870efecba50b73cc2a2db937e3d7959f861
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2010-03-23 20:13:11 +0000

    Add support for formatting of time durations.

--- lib/trunk/src/pagespeed/formatters/formatter_util.cc
+++ lib/trunk/src/pagespeed/formatters/formatter_util.cc
@@ -14,16 +14,68 @@
 
 #include "pagespeed/formatters/formatter_util.h"
 
+#include <algorithm>
+#include <vector>
+
 #include "base/string_util.h"
+
+namespace {
+
+const int kBytesPerKiB = 1 << 10;
+const int kBytesPerMiB = 1 << 20;
+
+struct DurationDescriptor {
+  int quantity;
+  const char *display_name;
+};
+
+// TODO: need to localize the display names when we add localization
+// support.
+const DurationDescriptor kQuantities[] = {
+  { 1000, "millisecond" },
+  { 60, "second" },
+  { 60, "minute" },
+  { 24, "hour" },
+  { 365, "day" },
+  { -1, "year" },
+};
+
+const size_t kQuantitiesSize = sizeof(kQuantities) / sizeof(kQuantities[0]);
+const char *kTimeDurationFormatStr = "%d %s%s";
+const char *kZeroSecondsStr = "0 seconds";
+const size_t kNumComponentsToDisplay = 2;
+
+void DoFormatTimeDuration(int64_t duration,
+                          std::vector<std::string> *components) {
+  for (int i = 0; i < kQuantitiesSize && duration > 0; ++i) {
+    const DurationDescriptor *desc = kQuantities + i;
+    int64_t value = duration;
+    duration /= desc->quantity;
+    if (desc->quantity > 0) {
+      // If we're not at the largest Duration type (years) then
+      // extract the quantity for this duration.
+      value %= desc->quantity;
+    }
+
+    if (value == 0) {
+      // Don't emit "0 seconds" or "0 minutes".
+      continue;
+    }
+
+    components->push_back(StringPrintf(kTimeDurationFormatStr,
+                                       static_cast<int>(value),
+                                       desc->display_name,
+                                       value != 1 ? "s": ""));
+  }
+}
+
+}  // namespace
 
 namespace pagespeed {
 
 namespace formatters {
 
-const int kBytesPerKiB = 1 << 10;
-const int kBytesPerMiB = 1 << 20;
-
-std::string FormatBytes(const int bytes) {
+std::string FormatBytes(int64_t bytes) {
   if (bytes < kBytesPerKiB) {
     return StringPrintf("%dB", bytes);
   } else if (bytes < kBytesPerMiB) {
@@ -31,6 +83,25 @@ std::string FormatBytes(const int bytes) {
   } else {
     return StringPrintf("%.1fMiB", bytes / static_cast<double>(kBytesPerMiB));
   }
+}
+
+std::string FormatTimeDuration(int64_t milliseconds) {
+  if (milliseconds == 0LL) {
+    // Special case when input is 0 millis.
+    return kZeroSecondsStr;
+  }
+  std::vector<std::string> components;
+  DoFormatTimeDuration(milliseconds, &components);
+
+  // DoFormatTimeDuration emits components from smallest time unit to
+  // largest time unit, so we need to reverse it.
+  std::reverse(components.begin(), components.end());
+
+  if (components.size() > kNumComponentsToDisplay) {
+    // Show at most kNumComponentsToDisplay.
+    components.resize(kNumComponentsToDisplay);
+  }
+  return JoinString(components, ' ');
 }
 
 }  // namespace formatters
