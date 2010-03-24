b56812aeef2501389117cc0b828cd765d6f6283c
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2010-03-24 01:29:06 +0000

    Add 2 new caching rules.

---
+++ lib/trunk/src/pagespeed/rules/specify_a_cache_validator.cc
@@ -0,0 +1,121 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "pagespeed/rules/specify_a_cache_validator.h"
+
+#include "base/logging.h"
+#include "pagespeed/core/formatter.h"
+#include "pagespeed/core/pagespeed_input.h"
+#include "pagespeed/core/resource.h"
+#include "pagespeed/core/resource_util.h"
+#include "pagespeed/proto/pagespeed_output.pb.h"
+
+namespace {
+
+bool HasValidLastModifiedHeader(const pagespeed::Resource& resource) {
+  const std::string& last_modified =
+      resource.GetResponseHeader("Last-Modified");
+  if (last_modified.empty()) {
+    return false;
+  }
+  int64_t last_modified_value = 0;
+  if (!pagespeed::resource_util::ParseTimeValuedHeader(
+          last_modified.c_str(), &last_modified_value)) {
+    return false;
+  }
+  return true;
+}
+
+bool HasETagHeader(const pagespeed::Resource& resource) {
+  const std::string& etag = resource.GetResponseHeader("ETag");
+  return !etag.empty();
+}
+
+}  // namespace
+
+namespace pagespeed {
+
+namespace rules {
+
+SpecifyACacheValidator::SpecifyACacheValidator() {
+}
+
+const char* SpecifyACacheValidator::name() const {
+  return "SpecifyACacheValidator";
+}
+
+const char* SpecifyACacheValidator::header() const {
+  return "Specify a cache validator";
+}
+
+const char* SpecifyACacheValidator::documentation_url() const {
+  return "caching.html#LeverageBrowserCaching";
+}
+
+bool SpecifyACacheValidator::AppendResults(const PagespeedInput& input,
+                                           Results* results) {
+  for (int i = 0, num = input.num_resources(); i < num; ++i) {
+    const Resource& resource = input.GetResource(i);
+    if (!resource_util::IsLikelyStaticResource(resource)) {
+      // Probably not a static resource, so don't suggest using a
+      // cache validator.
+      continue;
+    }
+
+    if (HasValidLastModifiedHeader(resource) ||
+        HasETagHeader(resource)) {
+      // The response already has a valid cache validator.
+      continue;
+    }
+
+    Result* result = results->add_results();
+    result->set_rule_name(name());
+
+    // TODO: populate savings.
+
+    result->add_resource_urls(resource.GetRequestUrl());
+  }
+  return true;
+}
+
+void SpecifyACacheValidator::FormatResults(const ResultVector& results,
+                                           Formatter* formatter) {
+  if (results.empty()) {
+    return;
+  }
+
+  Formatter* body = formatter->AddChild(
+      "The following resources are missing a cache validator. Resources "
+      "that do not specify a cache validator cannot be refreshed efficiently. "
+      "Specify a Last-Modified or ETag header to enable cache validation "
+      "for the following resources:");
+
+  for (ResultVector::const_iterator iter = results.begin(),
+           end = results.end();
+       iter != end;
+       ++iter) {
+    const Result& result = **iter;
+    if (result.resource_urls_size() != 1) {
+      LOG(DFATAL) << "Unexpected number of resource URLs.  Expected 1, Got "
+                  << result.resource_urls_size() << ".";
+      continue;
+    }
+    Argument url(Argument::URL, result.resource_urls(0));
+    body->AddChild("$1", url);
+  }
+}
+
+}  // namespace rules
+
+}  // namespace pagespeed
---
+++ lib/trunk/src/pagespeed/rules/specify_a_cache_validator_test.cc
@@ -0,0 +1,115 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+
+#include "base/scoped_ptr.h"
+#include "pagespeed/core/pagespeed_input.h"
+#include "pagespeed/core/resource.h"
+#include "pagespeed/proto/pagespeed_output.pb.h"
+#include "pagespeed/rules/specify_a_cache_validator.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using pagespeed::rules::SpecifyACacheValidator;
+using pagespeed::PagespeedInput;
+using pagespeed::Resource;
+using pagespeed::Result;
+using pagespeed::Results;
+using pagespeed::Savings;
+
+namespace {
+
+class SpecifyACacheValidatorTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    input_.reset(new PagespeedInput);
+  }
+
+  virtual void TearDown() {
+    input_.reset();
+  }
+
+  void AddTestResource(const char* url,
+                       const char* last_modified_header) {
+    Resource* resource = new Resource;
+    resource->SetRequestUrl(url);
+    resource->SetRequestMethod("GET");
+    resource->SetResponseStatusCode(200);
+    resource->AddResponseHeader("Content-Type", "image/png");
+    if (last_modified_header != NULL) {
+      resource->AddResponseHeader("Last-Modified", last_modified_header);
+    }
+    input_->AddResource(resource);
+  }
+
+  void CheckNoViolations() {
+    SpecifyACacheValidator rule;
+
+    Results results;
+    ASSERT_TRUE(rule.AppendResults(*input_, &results));
+    ASSERT_EQ(0, results.results_size());
+  }
+
+  void CheckOneViolation(const char *url) {
+    SpecifyACacheValidator rule;
+
+    Results results;
+    ASSERT_TRUE(rule.AppendResults(*input_, &results));
+    ASSERT_EQ(1, results.results_size());
+
+    const Result& result0 = results.results(0);
+    ASSERT_EQ(result0.resource_urls_size(), 1);
+    ASSERT_EQ(result0.resource_urls(0), url);
+  }
+
+  scoped_ptr<PagespeedInput> input_;
+};
+
+TEST_F(SpecifyACacheValidatorTest, MissingCacheValidator) {
+  AddTestResource("http://www.example.com/", NULL);
+  ASSERT_EQ(1, input_->num_resources());
+  CheckOneViolation("http://www.example.com/");
+}
+
+TEST_F(SpecifyACacheValidatorTest, HasCacheValidator) {
+  AddTestResource("http://www.example.com/1",
+                  "Thu, 18 Mar 2010 10:36:52 EDT");
+  ASSERT_EQ(1, input_->num_resources());
+  CheckNoViolations();
+}
+
+TEST_F(SpecifyACacheValidatorTest, InvalidCacheValidator) {
+  AddTestResource("http://www.example.com/1", "0");
+  ASSERT_EQ(1, input_->num_resources());
+  CheckOneViolation("http://www.example.com/1");
+}
+
+TEST_F(SpecifyACacheValidatorTest, ExplicitNoCacheDirective) {
+  Resource* resource = new Resource;
+  resource->SetRequestUrl("http://www.example.com/");
+  resource->SetRequestMethod("GET");
+  resource->AddResponseHeader("Content-Type", "image/png");
+  resource->SetResponseStatusCode(200);
+  input_->AddResource(resource);
+
+  // This resource should cause a violation.
+  CheckOneViolation("http://www.example.com/");
+
+  // Now add a no-cache directive. We expect the resource to no longer
+  // cause a violation.
+  resource->AddResponseHeader("Pragma", "no-cache");
+  CheckNoViolations();
+}
+
+}  // namespace
