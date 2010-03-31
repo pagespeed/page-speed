a99c0e50d534d659ba71daf0557e08465712cb65
bmcquade <bmcquade@google.com>
2010-03-31 15:48:48 +0000

    Create 1.7.0 firefox addon branch.

---
+++ firefox_addon/branches/page-speed-1.7.0/src/pagespeed_firefox/cpp/activity/clock.h
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2008-2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: Bryan McQuade
+//
+// Clock interface and implementation. ClockInterface should be used
+// in classes that want to get the current time, in order to make
+// those classes more testable. In production, use the Clock
+// implementation. In tests, use the MockClock implementation.
+
+#ifndef CLOCK_H_
+#define CLOCK_H_
+
+#include "base/basictypes.h"
+
+namespace activity {
+
+class ClockInterface {
+ public:
+  ClockInterface();
+  virtual ~ClockInterface();
+
+  virtual int64 GetCurrentTimeUsec() = 0;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ClockInterface);
+};
+
+// A real clock implementation that returns the current time.
+class Clock : public ClockInterface {
+ public:
+  Clock();
+  virtual ~Clock();
+
+  virtual int64 GetCurrentTimeUsec();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(Clock);
+};
+
+}  // namespace activity
+
+
+namespace activity_testing {
+
+// Simple mock implementation that increments the clock on each call
+// to GetCurrentTimeUsec(). Should be used only for testing.
+class MockClock : public activity::ClockInterface {
+ public:
+  MockClock();
+  virtual ~MockClock();
+
+  virtual int64 GetCurrentTimeUsec();
+
+  int64 current_time_usec_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockClock);
+};
+
+}  // namespace activity_testing
+
+#endif  // CLOCK_H_
