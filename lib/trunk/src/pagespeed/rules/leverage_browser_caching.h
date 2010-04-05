5e606d4824c6d8ae8beaae06d2c27d155063665c
bmcquade <bmcquade@google.com>
2010-04-05 17:14:27 +0000

    Consolidate 2 caching rules into one, and remove SpecifyADateHeader.

--- lib/trunk/src/pagespeed/rules/leverage_browser_caching.h
+++ lib/trunk/src/pagespeed/rules/leverage_browser_caching.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef PAGESPEED_RULES_SPECIFY_A_DATE_HEADER_H_
-#define PAGESPEED_RULES_SPECIFY_A_DATE_HEADER_H_
+#ifndef PAGESPEED_RULES_LEVERAGE_BROWSER_CACHING_H_
+#define PAGESPEED_RULES_LEVERAGE_BROWSER_CACHING_H_
 
 #include "base/basictypes.h"
 #include "pagespeed/core/rule.h"
@@ -26,22 +26,11 @@ class Results;
 namespace rules {
 
 /**
- * Checks for requests that are missing a date header. The Date header
- * is a required HTTP/1.1 response header, and according to the
- * HTTP/1.1 RFC, it is necessary in order to compute the freshness
- * lifetime of a resource, which is needed in order to determine if a
- * cached resource is valid. Currently, the major browsers will use
- * the resource response time if a Date header is not provided, but
- * previous releases of some browsers have failed to optimally cache
- * content that is missing a date header. In addition, all versions of
- * the Squid HTTP proxy up to 3.0.STABLE1 will not cache resources
- * that are missing a date header, even when a future Cache-Control
- * max-age is specified. This behavior is consistent with the HTTP
- * RFC. The short summary is: always specify a valid date header.
+ * Checks for requests that have a short freshness lifetime.
  */
-class SpecifyADateHeader : public Rule {
+class LeverageBrowserCaching : public Rule {
  public:
-  SpecifyADateHeader();
+  LeverageBrowserCaching();
 
   // Rule interface.
   virtual const char* name() const;
@@ -50,12 +39,14 @@ class SpecifyADateHeader : public Rule {
   virtual bool AppendResults(const PagespeedInput& input,
                              ResultProvider* provider);
   virtual void FormatResults(const ResultVector& results, Formatter* formatter);
+  virtual int ComputeScore(const InputInformation& input_info,
+                           const ResultVector& results);
 
-  DISALLOW_COPY_AND_ASSIGN(SpecifyADateHeader);
+  DISALLOW_COPY_AND_ASSIGN(LeverageBrowserCaching);
 };
 
 }  // namespace rules
 
 }  // namespace pagespeed
 
-#endif  // PAGESPEED_RULES_SPECIFY_A_DATE_HEADER_H_
+#endif  // PAGESPEED_RULES_LEVERAGE_BROWSER_CACHING_H_
