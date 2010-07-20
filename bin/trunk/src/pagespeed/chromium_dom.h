842830c833227564edc731a2d08e8d40e1ecbb47
bmcquade <bmcquade@google.com>
2010-07-20 13:30:02 +0000

    Fix lint issues.

--- bin/trunk/src/pagespeed/chromium_dom.h
+++ bin/trunk/src/pagespeed/chromium_dom.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef PAGESPEED_PAGESPEED_CHROMIUM_DOM_H_
-#define PAGESPEED_PAGESPEED_CHROMIUM_DOM_H_
+#ifndef PAGESPEED_CHROMIUM_DOM_H_
+#define PAGESPEED_CHROMIUM_DOM_H_
 
 namespace WebKit {
 class WebDocument;
@@ -31,4 +31,4 @@ DomDocument* CreateDocument(const WebKit::WebDocument& document);
 
 }  // namespace pagespeed
 
-#endif  // PAGESPEED_PAGESPEED_CHROMIUM_DOM_H_
+#endif  // PAGESPEED_CHROMIUM_DOM_H_
