9475cf146f7abbeee4afb87427f43cf8df44601a
bmcquade <bmcquade@google.com>
2010-08-16 19:26:05 +0000

    Remove jsmin xpcom component (no longer used).

--- firefox_addon/trunk/src/pagespeed_firefox/cpp/pagespeed/pagespeed_module.cc
+++ firefox_addon/trunk/src/pagespeed_firefox/cpp/pagespeed/pagespeed_module.cc
@@ -17,38 +17,25 @@
 // Author: Bryan McQuade
 // Author: Tony Gentilcore
 
-#include "js_min/js_minifier.h"
-#ifdef PAGESPEED_INCLUDE_LIBRARY_RULES
 #include "pagespeed/pagespeed_rules.h"
-#endif
 
 #include "nsIGenericFactory.h"
 
 namespace pagespeed {
-#ifdef PAGESPEED_INCLUDE_LIBRARY_RULES
 NS_GENERIC_FACTORY_CONSTRUCTOR(PageSpeedRules)
-#endif
-NS_GENERIC_FACTORY_CONSTRUCTOR(JsMinifier)
 }  // namespace pagespeed
 
 namespace {
 
-#ifdef PAGESPEED_INCLUDE_LIBRARY_RULES
 const char *PAGE_SPEED_RULES_CONTRACTID =
     "@code.google.com/p/page-speed/PageSpeedRules;1";
 const char *PAGE_SPEED_RULES_CLASSNAME = "PageSpeedRules";
-#endif
-
-const char *JS_MINIFIER_CONTRACTID =
-    "@code.google.com/p/page-speed/JsMin;1";
-const char *JS_MINIFIER_CLASSNAME = "JsMinifier";
 
 // CIDs, or "class identifiers", are used by xpcom to uniquely
 // identify a class or component. See
 // http://www.mozilla.org/projects/xpcom/book/cxc/html/quicktour2.html#1005329
 // for more information.
 
-#ifdef PAGESPEED_INCLUDE_LIBRARY_RULES
 #define PAGE_SPEED_RULES_CID                             \
 { /* 9d5c2098-b43c-4874-a12a-57c4b93896aa */             \
      0x9d5c2098,                                         \
@@ -56,38 +43,13 @@ const char *JS_MINIFIER_CLASSNAME = "JsMinifier";
      0x4874,                                             \
      { 0xa1, 0x2a, 0x57, 0xc4, 0xb9, 0x38, 0x96, 0xaa }  \
 }
-#endif
-
-#define JS_MINIFIER_CID                               \
-{ /* 9e97eb52-2bea-4f77-9aa4-6e26640db987 */          \
-  0x9e97eb52,                                         \
-  0x2bea,                                             \
-  0x4f77,                                             \
-  { 0x9a, 0xa4, 0x6e, 0x26, 0x64, 0x0d, 0xb9, 0x87 }  \
-}
-
-#define PROFILER_CID                                     \
-{ /* 89cdb437-83b9-4544-ae85-7fb152458885 */             \
-     0x89cdb437,                                         \
-     0x83b9,                                             \
-     0x4544,                                             \
-     { 0xae, 0x85, 0x7f, 0xb1, 0x52, 0x45, 0x88, 0x85 }  \
-}
 
 static nsModuleComponentInfo components[] = {
-#ifdef PAGESPEED_INCLUDE_LIBRARY_RULES
   {
     PAGE_SPEED_RULES_CLASSNAME,
     PAGE_SPEED_RULES_CID,
     PAGE_SPEED_RULES_CONTRACTID,
     pagespeed::PageSpeedRulesConstructor,
-  },
-#endif
-  {
-    JS_MINIFIER_CLASSNAME,
-    JS_MINIFIER_CID,
-    JS_MINIFIER_CONTRACTID,
-    pagespeed::JsMinifierConstructor,
   },
 };
 
