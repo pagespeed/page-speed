f26c7cf813930e734207fbaa9c0a64f4c6afa987
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2010-08-23 14:18:13 +0000

    Add a field to record the document URL associated with the JS call.

--- lib/trunk/src/pagespeed/core/javascript_call_info.h
+++ lib/trunk/src/pagespeed/core/javascript_call_info.h
@@ -26,18 +26,44 @@ namespace pagespeed {
 // the application.
 class JavaScriptCallInfo {
  public:
+  // NOTE: Object arguments should be serialized to JSON by the
+  // caller. The caller should take care to only serialize the minimal
+  // subset of properties necessary in order to reduce memory
+  // overhead, and avoid attempting to serialize objects with cyclic
+  // references.
   JavaScriptCallInfo(const std::string& id,
+                     const std::string& document_url,
                      const std::vector<std::string>& args,
                      int line_number)
-      : id_(id), args_(args), line_number_(line_number) {}
+      : id_(id),
+        document_url_(document_url),
+        args_(args),
+        line_number_(line_number) {}
 
   // Identifier (e.g. the function name).
   const std::string& id() const { return id_; }
+
+  // URL of the document where this JavaScriptCallInfo was
+  // captured. Used to determine in which document context the JSCI was
+  // run (e.g. for resolving relative URLs).
+  const std::string& document_url() const { return document_url_; }
+
+  // List of arguments passed to the function. Object arguments will
+  // be serialized to JSON.
   const std::vector<std::string>& args() const { return args_; }
+
+  // The line number of the root JavaScript call (i.e. the function at
+  // the bottom of the call stack) where this JavaScriptCallInfo was
+  // captured. The line number is a line number in the source of the
+  // resource associated with this JavaScriptCallInfo (not necessarily
+  // the document_url()). For inline JS this will be line number in an
+  // HTML document; for external JS this will be a line number in the
+  // JS file.
   int line_number() const { return line_number_; }
 
  private:
   std::string id_;
+  std::string document_url_;
   std::vector<std::string> args_;
   int line_number_;
 
