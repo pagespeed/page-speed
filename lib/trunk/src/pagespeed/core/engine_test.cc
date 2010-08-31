1b6f5cdb93020de927d0537c5722d14924aef411
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2010-08-31 20:41:52 +0000

    Add ability to remove rules from ruleset for which the required data is unavailable.

--- lib/trunk/src/pagespeed/core/engine_test.cc
+++ lib/trunk/src/pagespeed/core/engine_test.cc
@@ -16,12 +16,13 @@
 #include <vector>
 
 #include "pagespeed/core/engine.h"
+#include "pagespeed/core/javascript_call_info.h"
 #include "pagespeed/core/pagespeed_input.h"
 #include "pagespeed/core/result_provider.h"
 #include "pagespeed/core/rule.h"
-#include "pagespeed/proto/pagespeed_output.pb.h"
 #include "pagespeed/formatters/proto_formatter.h"
-#include "testing/gtest/include/gtest/gtest.h"
+#include "pagespeed/proto/pagespeed_output.pb.h"
+#include "pagespeed/testing/pagespeed_test.h"
 
 using pagespeed::Argument;
 using pagespeed::Engine;
@@ -256,6 +257,69 @@ TEST(Engine, NonFrozenInputFails) {
   ASSERT_DEATH(engine.ComputeResults(input, &results),
                "Attempting to ComputeResults with non-frozen input.");
 #endif
+}
+
+class EstimateCapabilitiesBitmapTest
+    : public ::pagespeed_testing::PagespeedTest {};
+
+TEST_F(EstimateCapabilitiesBitmapTest, NotFrozen) {
+#ifdef NDEBUG
+  ASSERT_EQ(static_cast<uint32>(Rule::NONE),
+            Engine::EstimateCapabilitiesBitmap(*input()));
+#else
+  ASSERT_DEATH(Engine::EstimateCapabilitiesBitmap(*input()),
+               "Can't estimate capabilities of non-frozen input.");
+#endif
+}
+
+TEST_F(EstimateCapabilitiesBitmapTest, None) {
+  Freeze();
+  ASSERT_EQ(static_cast<uint32>(Rule::NONE),
+            Engine::EstimateCapabilitiesBitmap(*input()));
+}
+
+TEST_F(EstimateCapabilitiesBitmapTest, Dom) {
+  NewPrimaryResource("http://www.example.com/");
+  Freeze();
+  ASSERT_EQ(static_cast<uint32>(Rule::DOM | Rule::PARENT_CHILD_RESOURCE_MAP),
+            Engine::EstimateCapabilitiesBitmap(*input()));
+}
+
+TEST_F(EstimateCapabilitiesBitmapTest, JSCalls) {
+  std::vector<std::string> args;
+  New200Resource("http://www.example.com/")->AddJavaScriptCall(
+      new pagespeed::JavaScriptCallInfo("document.write",
+                                        "http://www.example.com/",
+                                        args,
+                                        1));
+  Freeze();
+  ASSERT_EQ(static_cast<uint32>(Rule::JS_CALLS_DOCUMENT_WRITE),
+            Engine::EstimateCapabilitiesBitmap(*input()));
+}
+
+TEST_F(EstimateCapabilitiesBitmapTest, LazyLoaded) {
+  New200Resource("http://www.example.com/")->SetLazyLoaded();
+  Freeze();
+  ASSERT_EQ(static_cast<uint32>(Rule::LAZY_LOADED),
+            Engine::EstimateCapabilitiesBitmap(*input()));
+}
+
+TEST_F(EstimateCapabilitiesBitmapTest, RequestHeaders) {
+  pagespeed::Resource* resource =
+      New200Resource("http://www.example.com/");
+  resource->AddRequestHeader("referer", "foo");
+  resource->AddRequestHeader("host", "foo");
+  resource->AddRequestHeader("accept-encoding", "foo");
+  Freeze();
+  ASSERT_EQ(static_cast<uint32>(Rule::REQUEST_HEADERS),
+            Engine::EstimateCapabilitiesBitmap(*input()));
+}
+
+TEST_F(EstimateCapabilitiesBitmapTest, ResponseBody) {
+  New200Resource("http://www.example.com/")->SetResponseBody("a");
+  Freeze();
+  ASSERT_EQ(static_cast<uint32>(Rule::RESPONSE_BODY),
+            Engine::EstimateCapabilitiesBitmap(*input()));
 }
 
 }  // namespace
