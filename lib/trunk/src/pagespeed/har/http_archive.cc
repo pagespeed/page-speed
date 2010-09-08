58b1208d469d9d2c2bb0459bd023ee32712e5712
mdsteele@google.com <mdsteele@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2010-09-08 17:33:54 +0000

    Make HAR parser use page timings to set LazyLoaded (issue 329)
    r=lsong

--- lib/trunk/src/pagespeed/har/http_archive.cc
+++ lib/trunk/src/pagespeed/har/http_archive.cc
@@ -14,8 +14,12 @@
 
 #include "pagespeed/har/http_archive.h"
 
+#include <ctype.h>  // for isdigit
+#include <stdio.h>  // for sscanf
+
 #include "base/basictypes.h"
 #include "base/logging.h"
+#include "base/third_party/nspr/prtime.h"
 #include "pagespeed/core/pagespeed_input.h"
 #include "third_party/cJSON/cJSON.h"
 #include "third_party/modp_b64/modp_b64.h"
@@ -31,10 +35,11 @@ class InputPopulator {
  private:
   enum HeaderType { REQUEST_HEADERS, RESPONSE_HEADERS };
 
-  InputPopulator() : error_(false) {}
+  InputPopulator() : error_(false), page_finished_millis_(-1) {}
   ~InputPopulator() {}
 
   void PopulateInput(cJSON* har_json, PagespeedInput* input);
+  void DeterminePageFinishedMillis(cJSON* log_json);
   void PopulateResource(cJSON* entry_json, Resource* resource);
   void PopulateHeaders(cJSON* headers_json, HeaderType htype,
                        Resource* resource);
@@ -42,6 +47,7 @@ class InputPopulator {
   const std::string GetString(cJSON *object, const char* key);
 
   bool error_;
+  int64 page_finished_millis_;
 
   DISALLOW_COPY_AND_ASSIGN(InputPopulator);
 };
@@ -67,7 +73,18 @@ void InputPopulator::PopulateInput(cJSON* har_json, PagespeedInput* input) {
     return;
   }
 
-  // TODO: Check HAR version to make sure we're compatible.
+  // We require HAR version 1.2 (or, perhaps, greater?), because we need the
+  // "encoding" field for resource content.  If the version field is not
+  // present, the HAR spec says that version 1.1 should be assumed.  See
+  // http://groups.google.com/group/http-archive-specification/web/har-1-2-spec
+  cJSON* version_json = cJSON_GetObjectItem(log_json, "version");
+  if (version_json == NULL || version_json->type != cJSON_String ||
+      strcmp(version_json->valuestring, "1.2")) {
+    INPUT_POPULATOR_ERROR() << "HAR version must be 1.2";
+    return;
+  }
+
+  DeterminePageFinishedMillis(log_json);
 
   cJSON* entries_json = cJSON_GetObjectItem(log_json, "entries");
   if (entries_json == NULL || entries_json->type != cJSON_Array) {
@@ -87,14 +104,63 @@ void InputPopulator::PopulateInput(cJSON* har_json, PagespeedInput* input) {
   }
 }
 
+void InputPopulator::DeterminePageFinishedMillis(cJSON* log_json) {
+  cJSON* pages_json = cJSON_GetObjectItem(log_json, "pages");
+  if (pages_json == NULL || pages_json->type != cJSON_Array) {
+    // The "pages" field is optional, so give up without error if it's not
+    // there.
+    return;
+  }
+
+  // For now, just take the first page (if any), and ignore others.
+  // TODO(mdsteele): Behave intelligently in the face of multiple pages.
+  cJSON* page_json = pages_json->child;
+  if (page_json == NULL) {
+    return;
+  } else if (page_json->type != cJSON_Object) {
+    INPUT_POPULATOR_ERROR() << "Page item must be an object.";
+    return;
+  }
+
+  cJSON* timing_json = cJSON_GetObjectItem(page_json, "pageTimings");
+  if (timing_json == NULL || timing_json->type != cJSON_Object) {
+    INPUT_POPULATOR_ERROR() << "\"pageTimings\" field must be an object.";
+    return;
+  }
+
+  cJSON* onload_json = cJSON_GetObjectItem(timing_json, "onLoad");
+  if (onload_json != NULL && onload_json->type == cJSON_Number) {
+    const std::string started_datetime =
+        GetString(page_json, "startedDateTime");
+    int64 started_millis;
+    const int64 onload_millis = static_cast<int64>(onload_json->valueint);
+    if (onload_millis >= 0 &&
+        Iso8601ToEpochMillis(started_datetime, &started_millis)) {
+      page_finished_millis_ = started_millis + onload_millis;
+    }
+  }
+}
+
 void InputPopulator::PopulateResource(cJSON* entry_json, Resource* resource) {
   if (entry_json->type != cJSON_Object) {
     INPUT_POPULATOR_ERROR() << "Entry item must be an object.";
     return;
   }
 
-  // TODO: Use timings fields to determine if the resource was lazy-loaded.
+  // Determine if the resource was lazy-loaded.
+  {
+    cJSON* started_json = cJSON_GetObjectItem(entry_json, "startedDateTime");
+    if (started_json != NULL && started_json->type == cJSON_String) {
+      int64 started_millis;
+      if (Iso8601ToEpochMillis(started_json->valuestring, &started_millis)) {
+        if (started_millis > page_finished_millis_) {
+          resource->SetLazyLoaded();
+        }
+      }
+    }
+  }
 
+  // Get the request information.
   {
     cJSON* request_json = cJSON_GetObjectItem(entry_json, "request");
     if (request_json == NULL || request_json->type != cJSON_Object) {
@@ -118,6 +184,7 @@ void InputPopulator::PopulateResource(cJSON* entry_json, Resource* resource) {
     }
   }
 
+  // Get the response information.
   {
     cJSON* response_json = cJSON_GetObjectItem(entry_json, "response");
     if (response_json == NULL || response_json->type != cJSON_Object) {
@@ -257,6 +324,84 @@ PagespeedInput* ParseHttpArchiveWithFilter(const std::string& har_data,
 
 PagespeedInput* ParseHttpArchive(const std::string& har_data) {
   return ParseHttpArchiveWithFilter(har_data, NULL);
+}
+
+// TODO: It would be nice to have a more robust ISO 8601 parser here, but this
+// one seems to do okay for now on our unit tests.
+bool Iso8601ToEpochMillis(const std::string& input, int64* output) {
+  // We need to use unsigned ints, because otherwise sscanf() will look for +/-
+  // characters, which we do not want to allow.
+  unsigned int year, month, day, hours, minutes, seconds;
+  char tail[21];  // The tail of the string, for milliseconds and timezone.
+  // Parse the first six fields, and store the remainder of the string in tail.
+  // Fail if we don't successfully parse all the fields.
+  if (sscanf(input.c_str(), "%4u-%2u-%2uT%2u:%2u:%2u%20s",
+             &year, &month, &day, &hours, &minutes, &seconds, tail) != 7) {
+    return false;
+  }
+  // Fail if any of the fields so far are obviously out of range.
+  if (month < 1 || month > 12 || day < 1 || day > 31 ||
+      hours > 23 || minutes > 59 || seconds > 59) {
+    return false;
+  }
+
+  // Get the fractional part of the seconds, if any.  This is sort of ugly,
+  // because we have to interpret ".3" as ".300" and not ".003", so we can't
+  // just use sscanf.  Also, there may be more digits than we want
+  // (e.g. ".123456"), so this gracefully ignores the extra digits.  Of course,
+  // because tail is of static size, we'll still fail if there are dozens of
+  // digits of precision.  Oh, well.
+  int milliseconds = 0;
+  int index = 0;  // The current index into tail.
+  if (tail[0] == '.') {
+    int multiplier = 100;
+    index = 1;
+    while (isdigit(tail[index])) {
+      milliseconds += (tail[index] - '0') * multiplier;
+      multiplier /= 10;
+      ++index;
+    }
+  }
+  const int microseconds = PR_USEC_PER_MSEC * milliseconds;
+
+  // Now, index is pointing at the beginning of the timezone spec.  The
+  // timezone should be "Z" for UTC, or something like e.g. "-05:00" for EST.
+  int tz_offset_seconds = 0;
+  DCHECK(index < static_cast<int>(arraysize(tail)));
+  const char tz_sign = tail[index];
+  if (tz_sign == 'Z') {
+    // We're dealing with UTC.  Fail if the "Z" is not the last character of
+    // the string.
+    ++index;
+    DCHECK(index < static_cast<int>(arraysize(tail)));
+    if (tail[index] != '\0') {
+      return false;
+    }
+  } else if (tz_sign == '+' || tz_sign == '-') {
+    // We have a timezone offset.  Use sscanf to get the hours and minutes of
+    // the offset.  The 'ignored' char is a hack to make sure that this is the
+    // end of the string -- if sscanf doesn't parse exactly 2 out of the 3
+    // format arguments, then we fail.
+    unsigned int tz_hours, tz_minutes;
+    char ignored;
+    if (sscanf(tail + index + 1, "%2u:%2u%c",
+               &tz_hours, &tz_minutes, &ignored) != 2) {
+      return false;
+    }
+    tz_offset_seconds = ((tz_hours * 3600 + tz_minutes * 60) *
+                         (tz_sign == '+' ? 1 : -1));
+  } else {
+    // The timezone is invalid, so fail.
+    return false;
+  }
+
+  // Finally, use the prtime library to calculate the milliseconds since the
+  // epoch UTC for this datetime.
+  PRExplodedTime exploded = { microseconds, seconds, minutes, hours,
+                              day, month - 1, year, 0, 0,
+                              { tz_offset_seconds, 0 } };
+  *output = PR_ImplodeTime(&exploded) / PR_USEC_PER_MSEC;
+  return true;
 }
 
 }  // namespace pagespeed
--- lib/trunk/src/pagespeed/har/http_archive_test.cc
+++ lib/trunk/src/pagespeed/har/http_archive_test.cc
@@ -29,10 +29,22 @@ namespace {
 const std::string kHarInput = (
     "{"
     "  \"log\":{"
-    "    \"version\":\"1.1\","
+    "    \"version\":\"1.2\","
     "    \"creator\":{\"name\":\"http_archive_test\", \"version\":\"1.0\"},"
+    "    \"pages\":["
+    "      {"
+    "        \"startedDateTime\": \"2009-04-16T12:07:23.321Z\","
+    "        \"id\": \"page_0\","
+    "        \"title\": \"Example Page\","
+    "        \"pageTimings\": {"
+    "          \"onLoad\": 1500"
+    "        }"
+    "      }"
+    "    ],"
     "    \"entries\":["
     "      {"
+    "        \"pageref\": \"page_0\","
+    "        \"startedDateTime\": \"2009-04-16T12:07:23.596Z\","
     "        \"request\":{"
     "          \"method\":\"GET\","
     "          \"url\":\"http://www.example.com/index.html\","
@@ -61,6 +73,37 @@ const std::string kHarInput = (
     "          \"headersSize\":-1,"
     "          \"bodySize\":13"
     "        }"
+    "      },"
+    "      {"
+    "        \"pageref\": \"page_0\","
+    "        \"startedDateTime\": \"2009-04-16T12:07:25.596Z\","
+    "        \"request\":{"
+    "          \"method\":\"GET\","
+    "          \"url\":\"http://www.example.com/lazy.js\","
+    "          \"httpVersion\":\"HTTP/1.1\","
+    "          \"cookies\":[],"
+    "          \"headers\":[],"
+    "          \"headersSize\":-1,"
+    "          \"bodySize\":0"
+    "        },"
+    "        \"response\":{"
+    "          \"status\":200,"
+    "          \"statusText\":\"OK\","
+    "          \"httpVersion\":\"HTTP/1.1\","
+    "          \"cookies\":[],"
+    "          \"headers\":["
+    "            {\"name\":\"Content-Type\","
+    "             \"value\":\"application/javascript\"}"
+    "          ],"
+    "          \"content\":{"
+    "            \"size\":13,"
+    "            \"mimeType\":\"application/javascript\","
+    "            \"text\":\"Hello, world!\""
+    "          },"
+    "          \"redirectUrl\":\"\","
+    "          \"headersSize\":-1,"
+    "          \"bodySize\":13"
+    "        }"
     "      }"
     "    ]"
     "  }"
@@ -69,7 +112,7 @@ const std::string kHarInput = (
 const std::string kHarInputBase64 = (
     "{"
     "  \"log\":{"
-    "    \"version\":\"1.1\","
+    "    \"version\":\"1.2\","
     "    \"creator\":{\"name\":\"http_archive_test\", \"version\":\"1.0\"},"
     "    \"entries\":["
     "      {"
@@ -107,34 +150,50 @@ const std::string kHarInputBase64 = (
     "  }"
     "}");
 
-void AssertValidInput(const PagespeedInput* input) {
-  ASSERT_TRUE(input);
-
-  ASSERT_EQ(1, input->num_resources());
-  const Resource& resource = input->GetResource(0);
-
-  ASSERT_EQ("http://www.example.com/index.html", resource.GetRequestUrl());
-  ASSERT_EQ("GET", resource.GetRequestMethod());
-  ASSERT_EQ("bar", resource.GetRequestHeader("x-foo"));
-  ASSERT_EQ("", resource.GetRequestBody());
-
-  ASSERT_EQ(200, resource.GetResponseStatusCode());
-  ASSERT_EQ("text/html", resource.GetResponseHeader("content-type"));
-  ASSERT_EQ("Hello, world!", resource.GetResponseBody());
-
-  ASSERT_FALSE(resource.IsLazyLoaded());
-}
-
 TEST(HttpArchiveTest, ValidInput) {
   scoped_ptr<PagespeedInput> input(ParseHttpArchive(kHarInput));
+  ASSERT_FALSE(input == NULL);
   input->Freeze();
-  AssertValidInput(input.get());
+
+  ASSERT_EQ(2, input->num_resources());
+
+  const Resource& resource1 = input->GetResource(0);
+  EXPECT_EQ("http://www.example.com/index.html", resource1.GetRequestUrl());
+  EXPECT_EQ("GET", resource1.GetRequestMethod());
+  EXPECT_EQ("bar", resource1.GetRequestHeader("x-foo"));
+  EXPECT_EQ("", resource1.GetRequestBody());
+  EXPECT_EQ(200, resource1.GetResponseStatusCode());
+  EXPECT_EQ("text/html", resource1.GetResponseHeader("content-type"));
+  EXPECT_EQ("Hello, world!", resource1.GetResponseBody());
+  EXPECT_FALSE(resource1.IsLazyLoaded());
+
+  const Resource& resource2 = input->GetResource(1);
+  EXPECT_EQ("http://www.example.com/lazy.js", resource2.GetRequestUrl());
+  EXPECT_EQ("GET", resource2.GetRequestMethod());
+  EXPECT_EQ("", resource2.GetRequestBody());
+  EXPECT_EQ(200, resource2.GetResponseStatusCode());
+  EXPECT_EQ("application/javascript",
+            resource2.GetResponseHeader("content-type"));
+  EXPECT_EQ("Hello, world!", resource2.GetResponseBody());
+  EXPECT_TRUE(resource2.IsLazyLoaded());
 }
 
 TEST(HttpArchiveTest, ValidInputBase64) {
   scoped_ptr<PagespeedInput> input(ParseHttpArchive(kHarInputBase64));
+  ASSERT_FALSE(input == NULL);
   input->Freeze();
-  AssertValidInput(input.get());
+
+  ASSERT_EQ(1, input->num_resources());
+
+  const Resource& resource1 = input->GetResource(0);
+  EXPECT_EQ("http://www.example.com/index.html", resource1.GetRequestUrl());
+  EXPECT_EQ("GET", resource1.GetRequestMethod());
+  EXPECT_EQ("bar", resource1.GetRequestHeader("x-foo"));
+  EXPECT_EQ("", resource1.GetRequestBody());
+  EXPECT_EQ(200, resource1.GetResponseStatusCode());
+  EXPECT_EQ("text/html", resource1.GetResponseHeader("content-type"));
+  EXPECT_EQ("Hello, world!", resource1.GetResponseBody());
+  EXPECT_FALSE(resource1.IsLazyLoaded());
 }
 
 TEST(HttpArchiveTest, InvalidJSON) {
@@ -145,6 +204,58 @@ TEST(HttpArchiveTest, InvalidJSON) {
 TEST(HttpArchiveTest, InvalidHAR) {
   scoped_ptr<PagespeedInput> input(ParseHttpArchive("{\"log\":[]}"));
   ASSERT_EQ(NULL, input.get());
+}
+
+TEST(HttpArchiveTest, ValidIso8601) {
+  int64 millis = -1;
+  // Check some simple cases:
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-01T00:00:00Z",
+                                              &millis));
+  EXPECT_EQ(0, millis);
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-01T00:01:31Z",
+                                              &millis));
+  EXPECT_EQ(91000, millis);
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-01T00:01:30.000Z",
+                                              &millis));
+  EXPECT_EQ(90000, millis);
+  // Check several variations on the fractional part:
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-01T00:01:30.123Z",
+                                              &millis));
+  EXPECT_EQ(90123, millis);
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-01T00:01:30.89Z",
+                                              &millis));
+  EXPECT_EQ(90890, millis);
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-01T00:01:30.6Z",
+                                              &millis));
+  EXPECT_EQ(90600, millis);
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-01T00:01:30.56712Z",
+                                              &millis));
+  EXPECT_EQ(90567, millis);
+  // Try out various timezone offsets:
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-02T00:00:35.8Z",
+                                              &millis));
+  EXPECT_EQ(86435800, millis);
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-02T00:00:35.8+03:00",
+                                              &millis));
+  EXPECT_EQ(75635800, millis);
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("1970-01-02T00:00:35.8-02:41",
+                                              &millis));
+  EXPECT_EQ(96095800, millis);
+  // Now try a nontrivial datetime:
+  EXPECT_TRUE(pagespeed::Iso8601ToEpochMillis("2010-09-07T15:39:50.044-04:00",
+                                              &millis));
+  EXPECT_EQ(1283888390044LL, millis);
+}
+
+TEST(HttpArchiveTest, InvalidIso8601) {
+  int64 millis = 0;
+  EXPECT_FALSE(pagespeed::Iso8601ToEpochMillis("", &millis));
+  EXPECT_FALSE(pagespeed::Iso8601ToEpochMillis("foobar", &millis));
+  EXPECT_FALSE(pagespeed::Iso8601ToEpochMillis("12:35:20", &millis));
+  EXPECT_FALSE(pagespeed::Iso8601ToEpochMillis("1970-01-02 00:00:35.8Z",
+                                               &millis));
+  EXPECT_FALSE(pagespeed::Iso8601ToEpochMillis("1970-01-02T00:00:35.8Q",
+                                               &millis));
 }
 
 }  // namespace
