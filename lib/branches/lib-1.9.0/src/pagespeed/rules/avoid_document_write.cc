0bc39174cf605da1243e9c76efb8782ece0e476b
bmcquade <bmcquade@google.com>
2010-09-10 15:30:53 +0000

    Integrate r1126 from trunk to branch.

--- lib/branches/lib-1.9.0/src/pagespeed/rules/avoid_document_write.cc
+++ lib/branches/lib-1.9.0/src/pagespeed/rules/avoid_document_write.cc
@@ -331,6 +331,7 @@ bool AvoidDocumentWrite::AppendResults(const PagespeedInput& input,
                                        ResultProvider* provider) {
   bool error = false;
   net_instaweb::GoogleMessageHandler message_handler;
+  message_handler.set_min_message_type(net_instaweb::kError);
   net_instaweb::HtmlParse html_parse(&message_handler);
   ExternalResourceFilter filter(&html_parse);
   html_parse.AddFilter(&filter);
