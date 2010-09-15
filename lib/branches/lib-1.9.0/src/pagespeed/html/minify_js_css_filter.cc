d48282ef25c5ce9ebd5f104e684d580ea4c2d395
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2010-09-15 17:41:57 +0000

    Merge r1142 from trunk to branch.

--- lib/branches/lib-1.9.0/src/pagespeed/html/minify_js_css_filter.cc
+++ lib/branches/lib-1.9.0/src/pagespeed/html/minify_js_css_filter.cc
@@ -14,10 +14,89 @@
 
 #include "pagespeed/html/minify_js_css_filter.h"
 
+#include <functional>
+
 #include "base/logging.h"
+#include "base/string_util.h"
 #include "net/instaweb/htmlparse/public/html_parse.h"
 #include "pagespeed/cssmin/cssmin.h"
 #include "third_party/jsmin/cpp/jsmin.h"
+
+namespace {
+
+const char* kSgmlCommentStart = "<!--";
+const char* kSgmlCommentEnd = "-->";
+const size_t kSgmlCommentStartLen = strlen(kSgmlCommentStart);
+const size_t kSgmlCommentEndLen = strlen(kSgmlCommentEnd);
+
+// Attempts to remove SGML comments (i.e. <!-- -->) wrapping the input
+// string.
+//
+// Historically, SGML comments have been used in script blocks to
+// prevent user agents that don't know about script blocks from
+// attempting to parse the contents of the script block. See
+// http://www.w3.org/TR/REC-html40/interact/scripts.html#h-18.3.2 for
+// more. However, script blocks have been a standard part of HTML for
+// a long time, so all user agents in use today should not require
+// SGML comments in script blocks. Thus, we remove them if they are
+// present.
+//
+// This function only removes SGML comments if those SGML comments
+// enclose the entire input. For isntance, if input is "<!-- foo -->",
+// this function will remove the SGML comments, whereas if input is
+// "hello <!-- foo --> world" this function will not remove the
+// comments, since they do not wrap the entire input. Returns true if
+// comments were found and removed, false otherwise. The out parameter
+// is only valid if this function returns true.
+bool RemoveWrappingSgmlComments(const std::string& in, std::string* out) {
+  size_t sgml_comment_start = in.find(kSgmlCommentStart);
+  if (sgml_comment_start == in.npos) {
+    return false;
+  }
+  size_t sgml_comment_end = in.rfind(kSgmlCommentEnd);
+  if (sgml_comment_end == in.npos) {
+    return false;
+  }
+
+  // We've found both an opening and closing SGML comment marker. As
+  // long as the opening SGML comment is only preceded by whitespace,
+  // and the closing SGML comment is only followed by whitespace, we
+  // can remove them.
+
+  // stl::find_if operates on iterators, so convert our size_t offsets
+  // to iterators.
+  std::string::const_iterator sgml_comment_start_it =
+      in.begin() + sgml_comment_start;
+  std::string::const_iterator sgml_comment_end_it =
+      in.begin() + sgml_comment_end;
+
+  // See if there is any non-whitespace before the opening comment, or
+  // after the closing comment. std::find_if returns the first
+  // occurrence thta matches the predicate, or the end iterator if
+  // there was no match.
+  if (std::find_if(in.begin(), sgml_comment_start_it,
+                   std::not1(std::ptr_fun(IsAsciiWhitespace<char>))) !=
+      sgml_comment_start_it ||
+      std::find_if(sgml_comment_end_it + kSgmlCommentEndLen,
+                   in.end(),
+                   std::not1(std::ptr_fun(IsAsciiWhitespace<char>))) !=
+      in.end()) {
+    return false;
+  }
+
+  size_t first_newline_after_comment_start =
+      in.find('\n', sgml_comment_start + kSgmlCommentStartLen);
+
+  if (first_newline_after_comment_start == in.npos) {
+    out->erase();
+  } else {
+    out->assign(in.begin() + first_newline_after_comment_start,
+                sgml_comment_end_it);
+  }
+  return true;
+}
+
+}  // namespace
 
 namespace pagespeed {
 
@@ -34,24 +113,31 @@ void MinifyJsCssFilter::Characters(
   net_instaweb::HtmlElement* parent = characters->parent();
   if (parent != NULL) {
     net_instaweb::Atom tag = parent->tag();
+    bool did_minify = false;
+    std::string minified;
     if (tag == script_atom_) {
-      std::string minified;
-      if (jsmin::MinifyJs(characters->contents(), &minified)) {
-        html_parse_->ReplaceNode(
-            characters,
-            html_parse_->NewCharactersNode(characters->parent(), minified));
+      std::string contents;
+      if (RemoveWrappingSgmlComments(characters->contents(), &contents)) {
+        did_minify = jsmin::MinifyJs(contents, &minified);
       } else {
+        did_minify = jsmin::MinifyJs(characters->contents(), &minified);
+      }
+      if (!did_minify) {
         LOG(INFO) << "Inline JS minification failed.";
       }
     } else if (tag == style_atom_) {
-      std::string minified;
-      if (cssmin::MinifyCss(characters->contents(), &minified)) {
-        html_parse_->ReplaceNode(
-            characters,
-            html_parse_->NewCharactersNode(characters->parent(), minified));
-      } else {
+      // We do not currently strip SGML comments from CSS since CSS
+      // parsing behavior within CSS comments is inconsistent between
+      // browsers.
+      did_minify = cssmin::MinifyCss(characters->contents(), &minified);
+      if (!did_minify) {
         LOG(INFO) << "Inline CSS minification failed.";
       }
+    }
+    if (did_minify) {
+      html_parse_->ReplaceNode(
+          characters,
+          html_parse_->NewCharactersNode(characters->parent(), minified));
     }
   }
 }
