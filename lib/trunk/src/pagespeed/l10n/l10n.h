303022c4948d1f4a0264db226f6dd3b4102bc484
bmcquade <bmcquade@google.com>
2010-12-03 18:35:09 +0000

    Small fixes for l10n.h

--- lib/trunk/src/pagespeed/l10n/l10n.h
+++ lib/trunk/src/pagespeed/l10n/l10n.h
@@ -6,15 +6,15 @@
 
 #include "pagespeed/l10n/localizable_string.h"
 
-namespace pagespeed {
+#if defined(_) || defined(not_localized)
+#error localization macros are already defined.
+#endif
 
 // Macro for marking strings that need to be localized.  Any string
 // that should be localized should be surrounded by _(...).
-#define _(X) (LocalizableString(X))
+#define _(X) (::pagespeed::LocalizableString(X))
 
 // Macro for marking user-facing strings that should *not* be localized.
-#define not_localized(X) (LocalizableString(X))
-
-} // namespace pagespeed
+#define not_localized(X) (::pagespeed::LocalizableString(X))
 
 #endif  // PAGESPEED_L10N_L10N_H_
