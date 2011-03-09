ab73774700164396cb2545f6d0947f10657a1d8b
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2011-03-09 18:46:05 +0000

    Fix build.

--- lib/trunk/src/pagespeed/core/rule_input.cc
+++ lib/trunk/src/pagespeed/core/rule_input.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "pagespeed/rules/rule_input.h"
+#include "pagespeed/core/rule_input.h"
 
 #include <map>
 #include <set>
