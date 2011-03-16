facea17e006a38b200807287aae779061d30c3fe
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2011-03-16 14:07:26 +0000

    Create a 1.11 pre working branch.

---
+++ lib/branches/lib-1.11-pre/src/pagespeed/apps/optimize_image.cc
@@ -0,0 +1,158 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Command line utility to optimize images
+
+#include <stdio.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iterator>
+#include <string>
+
+#include "pagespeed/image_compression/gif_reader.h"
+#include "pagespeed/image_compression/jpeg_optimizer.h"
+#include "pagespeed/image_compression/png_optimizer.h"
+
+using pagespeed::image_compression::GifReader;
+using pagespeed::image_compression::OptimizeJpeg;
+using pagespeed::image_compression::PngOptimizer;
+using pagespeed::image_compression::PngReader;
+
+namespace {
+
+enum ImageType {
+  NOT_SUPPORTED,
+  JPEG,
+  PNG,
+  GIF,
+};
+
+const char *kUsage = "Usage: optimize_image <input> <output>\n";
+
+// use file extension to determine what optimizer should be used.
+ImageType DetermineImageType(const std::string& filename) {
+  size_t dot_pos = filename.rfind('.');
+  if (dot_pos != std::string::npos) {
+    std::string extension;
+    std::transform(filename.begin() + dot_pos + 1, filename.end(),
+                   std::back_insert_iterator<std::string>(extension),
+                   tolower);
+    if (extension == "jpg" || extension == "jpeg") {
+      return JPEG;
+    } else if (extension == "png") {
+      return PNG;
+    } else if (extension == "gif") {
+      return GIF;
+    }
+  }
+  return NOT_SUPPORTED;
+}
+
+bool OptimizeImage(const char* infile, const char* outfile) {
+  std::string filename(infile);
+  std::ifstream in(filename.c_str(), std::ios::in | std::ios::binary);
+  if (!in) {
+    fprintf(stderr, "Could not read input from %s\n", filename.c_str());
+    return false;
+  }
+
+  in.seekg (0, std::ios::end);
+  int length = in.tellg();
+  in.seekg (0, std::ios::beg);
+
+  char* buffer = new char[length];
+  in.read(buffer, length);
+  in.close();
+
+  std::string file_contents(buffer, length);
+  delete[] buffer;
+
+  std::string compressed;
+  ImageType type = DetermineImageType(filename);
+
+  bool success = false;
+  if (type == JPEG) {
+    success = OptimizeJpeg(file_contents, &compressed);
+  } else if (type == PNG) {
+    PngReader reader;
+    success = PngOptimizer::OptimizePngBestCompression(reader,
+                                                       file_contents,
+                                                       &compressed);
+  } else if (type == GIF) {
+    GifReader reader;
+    success = PngOptimizer::OptimizePngBestCompression(reader,
+                                                       file_contents,
+                                                       &compressed);
+  } else {
+    fprintf(stderr,
+            "Unsupported image type when processing %s\n",
+            filename.c_str());
+    return false;
+  }
+
+  if (!success) {
+    fprintf(stderr,
+            "Image compression failed when processing %s\n",
+            filename.c_str());
+    return false;
+  }
+
+  if (compressed.size() >= file_contents.size()) {
+    compressed = file_contents;
+  }
+
+  if (outfile != NULL) {
+    std::ofstream out(outfile, std::ios::out | std::ios::binary);
+    if (!out) {
+      fprintf(stderr, "Error opening %s for write\n", outfile);
+      return false;
+    }
+    out.write(compressed.c_str(), compressed.size());
+    out.close();
+  }
+
+  return true;
+}
+
+}  // namespace
+
+
+
+int main(int argc, char** argv) {
+  if (argc == 1) {
+    fprintf(stderr, "%s", kUsage);
+    return EXIT_SUCCESS;
+  }
+
+  // If running in batch mode, optimize every image specified on the
+  // command line. Do not write any optimized files to disk. This mode
+  // can be used to determine how long it takes to optimize a set of
+  // files.
+  if (strcmp("--batch", argv[1]) == 0) {
+    for (int i = 2; i < argc; ++i) {
+      OptimizeImage(argv[i], NULL);
+    }
+    return EXIT_SUCCESS;
+  }
+
+  // Otherwise we are running in normal mode, where the arguments are
+  // <infile> <outfile>.
+  if (argc != 3) {
+    fprintf(stderr, "%s", kUsage);
+    return EXIT_FAILURE;
+  }
+
+  return OptimizeImage(argv[1], argv[2]) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
---
+++ lib/branches/lib-1.11-pre/src/pagespeed/rules/make_landing_page_redirects_cacheable.cc
@@ -0,0 +1,146 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "pagespeed/rules/make_landing_page_redirects_cacheable.h"
+
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "base/logging.h"
+#include "googleurl/src/gurl.h"
+#include "googleurl/src/url_canon.h"
+#include "pagespeed/core/formatter.h"
+#include "pagespeed/core/pagespeed_input.h"
+#include "pagespeed/core/resource.h"
+#include "pagespeed/core/resource_util.h"
+#include "pagespeed/core/result_provider.h"
+#include "pagespeed/core/rule_input.h"
+#include "pagespeed/core/uri_util.h"
+#include "pagespeed/l10n/l10n.h"
+#include "pagespeed/proto/pagespeed_output.pb.h"
+
+namespace {
+
+const char* kRuleName = "MakeLandingPageRedirectsCacheable";
+
+bool GetNonCacheableRedirectsInRedirectChain(
+    const pagespeed::PagespeedInput& input,
+    const pagespeed::RuleInput::RedirectChain& chain,
+    pagespeed::ResourceVector* resources) {
+  for (pagespeed::RuleInput::RedirectChain::const_iterator it = chain.begin(),
+      end = chain.end();
+      it != end;
+      ++it) {
+    const pagespeed::Resource* resource =  *it;
+    if (resource->GetResponseStatusCode() != 301 &&
+        !pagespeed::resource_util::IsCacheableResource(*resource)) {
+      resources->push_back(resource);
+    }
+  }
+  return (!resources->empty());
+}
+
+}  // namespace
+
+namespace pagespeed {
+
+namespace rules {
+
+MakeLandingPageRedirectsCacheable::MakeLandingPageRedirectsCacheable()
+    : pagespeed::Rule(pagespeed::InputCapabilities()) {
+}
+
+const char* MakeLandingPageRedirectsCacheable::name() const {
+  return kRuleName;
+}
+
+UserFacingString MakeLandingPageRedirectsCacheable::header() const {
+  // TRANSLATOR: The name of a Page Speed rule that tells users to avoid
+  // non-cacheable redirects at the landing page. The landing page is the root
+  // HTML document that was requested the user in the browser's address bar.
+  // This is displayed in a list of rule names that Page Speed generates.
+  return _("Make landing page redirects cacheable");
+}
+
+const char* MakeLandingPageRedirectsCacheable::documentation_url() const {
+  return "rtt.html#AvoidRedirects";
+}
+
+bool MakeLandingPageRedirectsCacheable::AppendResults(
+    const RuleInput& rule_input, ResultProvider* provider) {
+  const PagespeedInput& input = rule_input.pagespeed_input();
+  // TODO(bmcquade): here to remove the fragment from the URL.
+  const std::string& primary_resource_url = input.primary_resource_url();
+
+  if (primary_resource_url.empty()) {
+    return false;
+  }
+  const Resource* primary_resource =
+      input.GetResourceWithUrl(primary_resource_url);
+
+  if (primary_resource == NULL) {
+    LOG(INFO) << "No resource for " << primary_resource_url;
+    return false;
+  }
+  const RuleInput::RedirectChain* chain =
+      rule_input.GetRedirectChainOrNull(primary_resource);
+  if (chain == NULL) {
+    return true;
+  }
+
+  ResourceVector resources;
+  if (!GetNonCacheableRedirectsInRedirectChain(input, *chain, &resources)) {
+    return true;
+  }
+
+  Result* result = provider->NewResult();
+  for (ResourceVector::const_iterator it = resources.begin(),
+      end = resources.end();
+      it != end;
+      ++it) {
+    result->add_resource_urls((*it)->GetRequestUrl());
+  }
+
+  pagespeed::Savings* savings = result->mutable_savings();
+  savings->set_requests_saved(resources.size());
+
+  return true;
+}
+
+void MakeLandingPageRedirectsCacheable::FormatResults(
+    const ResultVector& results, RuleFormatter* formatter) {
+  for (ResultVector::const_iterator iter = results.begin(),
+           end = results.end();
+       iter != end;
+       ++iter) {
+    UrlBlockFormatter* body = formatter->AddUrlBlock(
+        // TRANSLATOR: Header at the top of a list of URLs that Page Speed
+        // detected as a chain of HTTP redirections. It tells the user to fix
+        // the problem by removing the URLs that redirect to others.
+        _("The following landing page redirects are not cacheable. Make them"
+          "cacheable to speed up page load times for repeat visitors to your"
+          "site."));
+
+    const Result& result = **iter;
+    for (int url_idx = 0; url_idx < result.resource_urls_size(); url_idx++) {
+      body->AddUrl(result.resource_urls(url_idx));
+    }
+  }
+}
+
+}  // namespace rules
+
+}  // namespace pagespeed
