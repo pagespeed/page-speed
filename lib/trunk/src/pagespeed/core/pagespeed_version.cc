a9bbfebec497bbc2c3031575fa1ce1b8cdb9e5e7
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2011-03-17 19:05:48 +0000

    Bump library version to 1.11.

--- lib/trunk/src/pagespeed/core/pagespeed_version.cc
+++ lib/trunk/src/pagespeed/core/pagespeed_version.cc
@@ -19,7 +19,7 @@
 namespace {
 
 const int kPagespeedMajorVersion = 1;
-const int kPagespeedMinorVersion = 10;
+const int kPagespeedMinorVersion = 11;
 const bool kRelease = false;
 
 }  // namespace
