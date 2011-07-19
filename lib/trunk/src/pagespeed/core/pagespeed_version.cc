e974bb7f09b08c1fca7e59312c82445a3fcca6d8
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2011-07-19 15:32:43 +0000

    Update version to 1.12.

--- lib/trunk/src/pagespeed/core/pagespeed_version.cc
+++ lib/trunk/src/pagespeed/core/pagespeed_version.cc
@@ -19,7 +19,7 @@
 namespace {
 
 const int kPagespeedMajorVersion = 1;
-const int kPagespeedMinorVersion = 11;
+const int kPagespeedMinorVersion = 12;
 const bool kRelease = false;
 
 }  // namespace
