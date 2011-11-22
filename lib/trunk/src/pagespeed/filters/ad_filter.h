16f46edff9eb718c86d7827ddab93c5e81b84ce0
bmcquade <bmcquade@google.com>
2011-11-22 17:51:10 +0000

    Factor url regex filter into a common base class.

--- lib/trunk/src/pagespeed/filters/ad_filter.h
+++ lib/trunk/src/pagespeed/filters/ad_filter.h
@@ -15,25 +15,21 @@
 #ifndef PAGESPEED_FILTERS_AD_FILTER_H_
 #define PAGESPEED_FILTERS_AD_FILTER_H_
 
-#include <vector>
 #include "base/basictypes.h"
-#include "pagespeed/core/resource_filter.h"
-#include "pagespeed/util/regex.h"
+#include "pagespeed/filters/url_regex_filter.h"
 
 namespace pagespeed {
 
 /**
  * A ResourceFilter that filters out ads.
  */
-class AdFilter : public ResourceFilter {
+class AdFilter : public UrlRegexFilter {
  public:
   AdFilter();
   virtual ~AdFilter();
 
-  virtual bool IsAccepted(const Resource& resource) const;
-
  private:
-  RE url_regex_;
+  DISALLOW_COPY_AND_ASSIGN(AdFilter);
 };
 
 }  // namespace pagespeed
