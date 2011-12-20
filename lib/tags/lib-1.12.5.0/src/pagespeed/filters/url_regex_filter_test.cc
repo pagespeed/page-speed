44605e4da0fa4f1fb9ad6f9960bcede3d77112fe
bmcquade <bmcquade@google.com>
2011-12-20 18:23:59 +0000

    Create 1.12.5 release tag.

---
+++ lib/tags/lib-1.12.5.0/src/pagespeed/filters/url_regex_filter_test.cc
@@ -0,0 +1,47 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "pagespeed/filters/url_regex_filter.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+const char* kRegex = ".*www\\.example\\.com|.*foo\\.com/bar";
+
+TEST(UrlRegexFilterTest, UrlRegexFilter) {
+  pagespeed::Resource resource;
+  pagespeed::UrlRegexFilter filter(kRegex);
+
+  EXPECT_TRUE(filter.IsAccepted(resource));
+
+  resource.SetRequestUrl("http://www.google.com/");
+  EXPECT_TRUE(filter.IsAccepted(resource));
+
+  resource.SetRequestUrl("http://www.example.com/");
+  EXPECT_FALSE(filter.IsAccepted(resource));
+
+  resource.SetRequestUrl("http://www.example.com/foobar");
+  EXPECT_FALSE(filter.IsAccepted(resource));
+
+  resource.SetRequestUrl("http://other.foo.com/bar");
+  EXPECT_FALSE(filter.IsAccepted(resource));
+
+  resource.SetRequestUrl("http://other.foo.com/");
+  EXPECT_TRUE(filter.IsAccepted(resource));
+
+  resource.SetRequestUrl("http://other.foo.com/foo");
+  EXPECT_TRUE(filter.IsAccepted(resource));
+}
+
+}  // namespace
