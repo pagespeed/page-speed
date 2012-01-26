d1a170e7b21070a3231bdd09fb5d495d64b77abe
bmcquade <bmcquade@google.com>
2012-01-26 14:34:09 +0000

    Land updated version of NaCL SDK.

--- chromium_extension/trunk/src/makenacl.sh
+++ chromium_extension/trunk/src/makenacl.sh
@@ -14,33 +14,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Allow callers to override the nacl home directory.
 RELATIVE_PATH=$(dirname $0)
-DEFAULT_NACL_HOME="${RELATIVE_PATH}/third_party/naclsdk"
+
+DEFAULT_NACL_HOME="${RELATIVE_PATH}/third_party/nacl_sdk"
+DEFAULT_NACL_BUNDLE="pepper_17"
+
 if [ -z "$NACL_HOME" ]; then NACL_HOME=$DEFAULT_NACL_HOME; fi
+if [ -z "$NACL_BUNDLE" ]; then NACL_BUNDLE=$DEFAULT_NACL_BUNDLE; fi
 
 if [ ! -d "$NACL_HOME" ]; then
   echo "Unable to find NACL_HOME at $NACL_HOME."
   exit 1
 fi
 
-# Use the 32-bit toolchain by default.
-NACL_ARCH_DIR="nacl"
+./$NACL_HOME/naclsdk update $NACL_BUNDLE
+
+NACL_BIN_DIR=$NACL_HOME/$NACL_BUNDLE/toolchain/linux_x86/bin
+if [ ! -d "$NACL_BIN_DIR" ]; then
+  echo "Failed to find NaCL bin dir."
+  exit 1
+fi
 
 # Switch to the 64-bit toolchain if arch is x86_64.
-if [ $(uname -m) = "x86_64" ]; then NACL_ARCH_DIR="nacl64"; fi
+if [ $(uname -m) = "x86_64" ]; then
+  NACL_BIN_PREFIX="x86_64-nacl"
+else
+  NACL_BIN_PREFIX="i686-nacl"
+fi
 
-NACL_BIN=$NACL_HOME/toolchain/linux_x86/$NACL_ARCH_DIR/bin
 
-if [ ! -d "$NACL_BIN" ]; then
-  echo "Unable to find NACL_BIN at $NACL_BIN."
+if [ ! -d "$NACL_BIN_DIR" ]; then
+  echo "Unable to find NACL_BIN_DIR at $NACL_BIN_DIR."
   exit 1
 fi
 
 # Override the target toolchain used in the gyp-generated Makefile.
-make CC.target=$NACL_BIN/gcc         \
-     CXX.target=$NACL_BIN/g++        \
-     LINK.target=$NACL_BIN/g++       \
-     AR.target=$NACL_BIN/ar          \
-     RANLIB.target=$NACL_BIN/ranlib  \
+make CC.target=$NACL_BIN_DIR/$NACL_BIN_PREFIX-gcc         \
+     CXX.target=$NACL_BIN_DIR/$NACL_BIN_PREFIX-g++        \
+     LINK.target=$NACL_BIN_DIR/$NACL_BIN_PREFIX-g++       \
+     AR.target=$NACL_BIN_DIR/$NACL_BIN_PREFIX-ar          \
+     RANLIB.target=$NACL_BIN_DIR/$NACL_BIN_PREFIX-ranlib  \
      $@
