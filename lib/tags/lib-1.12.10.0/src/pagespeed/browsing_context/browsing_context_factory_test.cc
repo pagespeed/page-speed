df4845d5d559d404c592a0737d559cedc86eba75
bmcquade <bmcquade@google.com>
2012-05-30 18:43:44 +0000

    Create 1.12.10 library release.

---
+++ lib/tags/lib-1.12.10.0/src/pagespeed/browsing_context/browsing_context_factory_test.cc
@@ -0,0 +1,66 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+
+#include "base/scoped_ptr.h"
+#include "pagespeed/core/browsing_context.h"
+#include "pagespeed/browsing_context/browsing_context_factory.h"
+#include "pagespeed/testing/pagespeed_test.h"
+
+using pagespeed::browsing_context::BrowsingContextFactory;
+using pagespeed_testing::FakeDomDocument;
+using pagespeed_testing::FakeDomElement;
+
+namespace pagespeed {
+
+namespace {
+
+class BrowsingContextFactoryTest : public ::pagespeed_testing::PagespeedTest {};
+
+TEST_F(BrowsingContextFactoryTest, FindResources) {
+  Resource* primary = NewPrimaryResource("http://www.example.com/");
+  CreateHtmlHeadBodyElements();
+
+  NewScriptResource("http://www.example.com/script.js", head(), NULL);
+
+  FakeDomElement* iframe = FakeDomElement::NewIframe(body());
+  FakeDomDocument* iframe_document = NULL;
+  NewDocumentResource(
+      "http://www.example.com/iframe.html", iframe, &iframe_document);
+
+  BrowsingContextFactory context_factory(pagespeed_input());
+  SetTopLevelBrowsingContext(
+      context_factory.CreateTopLevelBrowsingContext(document(), primary));
+  Freeze();
+
+  const TopLevelBrowsingContext* top_level_context =
+      pagespeed_input()->GetTopLevelBrowsingContext();
+
+  ResourceVector top_level_resources;
+  top_level_context->GetResources(&top_level_resources);
+  ASSERT_EQ(static_cast<size_t>(2), top_level_resources.size());
+
+  ASSERT_EQ(1, top_level_context->GetNestedContextCount());
+
+  const BrowsingContext& nested = top_level_context->GetNestedContext(0);
+
+  ResourceVector nested_resources;
+  nested.GetResources(&nested_resources);
+  ASSERT_EQ(static_cast<size_t>(1), nested_resources.size());
+}
+
+}  // namespace
+
+}  // namespace pagespeed
---
+++ lib/tags/lib-1.12.10.0/src/pagespeed/html/external_resource_filter.cc
@@ -0,0 +1,117 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "pagespeed/html/external_resource_filter.h"
+
+#include <set>
+#include <string>
+#include "base/string_util.h"
+#include "net/instaweb/htmlparse/public/empty_html_filter.h"
+#include "net/instaweb/htmlparse/public/html_name.h"
+#include "net/instaweb/htmlparse/public/html_parse.h"
+#include "pagespeed/core/uri_util.h"
+
+namespace pagespeed {
+
+namespace {
+
+void ResolveExternalResourceUrls(
+    std::vector<std::string>* external_resource_urls,
+    const DomDocument* document,
+    const std::string& document_url) {
+  // Resolve URLs relative to their document.
+  for (std::vector<std::string>::iterator
+           it = external_resource_urls->begin(),
+           end = external_resource_urls->end();
+       it != end;
+       ++it) {
+    std::string resolved_uri;
+    if (!uri_util::ResolveUriForDocumentWithUrl(*it,
+                                                document,
+                                                document_url,
+                                                &resolved_uri)) {
+      // We failed to resolve relative to the document, so try to
+      // resolve relative to the document's URL. This will be
+      // correct unless the document contains a <base> tag.
+      resolved_uri = uri_util::ResolveUri(*it, document_url);
+    }
+    *it = resolved_uri;
+  }
+}
+
+}  // namespace
+
+namespace html {
+
+ExternalResourceFilter::ExternalResourceFilter(
+    net_instaweb::HtmlParse* html_parse) {
+}
+
+ExternalResourceFilter::~ExternalResourceFilter() {}
+
+void ExternalResourceFilter::StartDocument() {
+  external_resource_urls_.clear();
+}
+
+void ExternalResourceFilter::StartElement(net_instaweb::HtmlElement* element) {
+  net_instaweb::HtmlName::Keyword keyword = element->keyword();
+  if (keyword == net_instaweb::HtmlName::kScript ||
+      keyword == net_instaweb::HtmlName::kIframe ||
+      keyword == net_instaweb::HtmlName::kImg) {
+    const char* src = element->AttributeValue(net_instaweb::HtmlName::kSrc);
+    if (src != NULL) {
+      external_resource_urls_.push_back(src);
+    }
+    return;
+  }
+
+  if (keyword == net_instaweb::HtmlName::kLink) {
+    const char* rel = element->AttributeValue(net_instaweb::HtmlName::kRel);
+    if (rel == NULL || !LowerCaseEqualsASCII(rel, "stylesheet")) {
+      return;
+    }
+    const char* href = element->AttributeValue(net_instaweb::HtmlName::kHref);
+    if (href != NULL) {
+      external_resource_urls_.push_back(href);
+    }
+    return;
+  }
+}
+
+bool ExternalResourceFilter::GetExternalResourceUrls(
+    std::vector<std::string>* out,
+    const DomDocument* document,
+    const std::string& document_url) const {
+  // We want to uniqueify the list of URLs. The easiest way to do that
+  // is to copy the contents of the vector to a set, and then back to
+  // a vector.
+  std::vector<std::string> tmp(external_resource_urls_);
+  ResolveExternalResourceUrls(
+      &tmp, document, document_url);
+  std::set<std::string> unique;
+  for (std::vector<std::string>::const_iterator it = tmp.begin(),
+           end = tmp.end(); it != end; ++it) {
+    // Only include URLs for external resources (e.g. do not include
+    // data: URLs).
+    if (pagespeed::uri_util::IsExternalResourceUrl(*it)) {
+      unique.insert(*it);
+    }
+  }
+  out->assign(unique.begin(), unique.end());
+  return !out->empty();
+}
+
+}  // namespace html
+
+}  // namespace pagespeed
