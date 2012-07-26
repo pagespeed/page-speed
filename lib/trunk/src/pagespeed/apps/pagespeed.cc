8c3a4ef33635159e25d2cc43f851041593d41345
bmcquade@google.com <bmcquade@google.com@50e4380a-3672-11de-8ef0-0f53c1b7e458>
2012-07-26 14:37:49 +0000

    Two fixes necessary to get builds working on Windows.

--- lib/trunk/src/pagespeed/apps/pagespeed.cc
+++ lib/trunk/src/pagespeed/apps/pagespeed.cc
@@ -480,8 +480,16 @@ int main(int argc, char** argv) {
     log_destination = logging::LOG_TO_BOTH_FILE_AND_SYSTEM_DEBUG_LOG;
   }
 
+// logging::InitLogging expects a wchar_t* on windows, and char* on
+// other platforms.
+#if defined (OS_WIN)
+  std::wstring log_file_path(FLAGS_log_file.c_str());
+#else
+  std::string log_file_path(FLAGS_log_file.c_str());
+#endif
+
   logging::InitLogging(
-      FLAGS_log_file.c_str(),
+      log_file_path.c_str(),
       log_destination,
       // Since we are entirely single-threaded no need to lock the log file.
       logging::DONT_LOCK_LOG_FILE,
