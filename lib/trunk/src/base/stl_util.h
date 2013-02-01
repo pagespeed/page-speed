4f4f93238a714f83b411bd73065e14f3b30ac7c5
bmcquade <bmcquade@google.com>
2013-02-01 01:32:56 +0000

    Remove old stl_util.h stub which is no longer needed.

--- lib/trunk/src/base/stl_util.h
+++
@@ -1,4 +0,0 @@
-// This is a temporary file, used during our transition between
-// Chromium revisions. It will be deleted as soon as we migrate to a
-// new Chromium revision.
-#include "base/stl_util-inl.h"
