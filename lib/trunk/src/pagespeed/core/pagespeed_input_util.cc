c492d0359ff827519e3659deaa6028ac0b878e46
mdsteele <mdsteele@google.com>
2013-05-31 13:57:36 +0000

    Add render_blocking_round_trips_saved Savings field
    R=bmcquade
    http://page-speed-codereview.appspot.com/869002/

--- lib/trunk/src/pagespeed/core/pagespeed_input_util.cc
+++ lib/trunk/src/pagespeed/core/pagespeed_input_util.cc
@@ -22,17 +22,27 @@ namespace pagespeed_input_util {
 
 void PopulateMobileClientCharacteristics(
     pagespeed::ClientCharacteristics* client_characteristics) {
+  // Clear the proto, which resets all its fields to their default values,
+  // which are the values for desktop clients.  We can then apply the
+  // multipliers below for certain fields to get the values we want for mobile
+  // clients.
   client_characteristics->Clear();
 
   // 5x the desktop value
-  client_characteristics->set_javascript_parse_weight(0.00005);
+  client_characteristics->set_javascript_parse_weight(
+      5.0 * client_characteristics->javascript_parse_weight());
 
   // 1.5x the desktop value
-  client_characteristics->set_dns_requests_weight(3.0);
-  client_characteristics->set_requests_weight(1.5);
+  client_characteristics->set_dns_requests_weight(
+      1.5 * client_characteristics->dns_requests_weight());
+  client_characteristics->set_requests_weight(
+      1.5 * client_characteristics->requests_weight());
+
+  // 4x the desktop value
+  client_characteristics->set_render_blocking_round_trips_weight(
+      4.0 * client_characteristics->render_blocking_round_trips_weight());
 }
 
 }  // namespace pagespeed_input_util
 
 }  // namespace pagespeed
-
